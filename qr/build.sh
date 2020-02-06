2b7f07c9fd8a4b13fb752b304c71a1760304aeeb
Ben Lynn <benlynn@gmail.com>
2020-02-06 15:58:06 -0800

    Add `adventure` example.

--- qr/build.sh
+++ qr/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 if ! [ -a QR-Code-generator]; then
   git clone https://github.com/nayuki/QR-Code-generator
@@ -6,9 +7,8 @@ if ! [ -a QR-Code-generator]; then
   git checkout fd6917601d0a77b6b6df81599118212d8cdb9a27
   cd ..
 fi
-
 wld="wasm-ld-8 --no-entry --export-dynamic --allow-undefined"
-wcc="clang --target=wasm32 -c -O3"
+wcc="clang-8 --target=wasm32 -c -O3"
 $wcc -DNDEBUG -I QR-Code-generator/c QR-Code-generator/c/qrcodegen.c qr.c
 $wld qr.o qrcodegen.o -o qr.wasm
 echo '{"canisters":{"qr":{"main":"src/qr"}}}' > dfx.json
