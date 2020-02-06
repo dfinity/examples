67ef59ad8f7e3465cf8e4d3d2421d073af621648
Ben Lynn <benlynn@gmail.com>
2020-02-06 12:13:48 -0800

    Add QR code example.
    Fix compilation warnings in `reverse` demo.

---
+++ qr/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if ! [ -a QR-Code-generator]; then
+  git clone https://github.com/nayuki/QR-Code-generator
+  cd QR-Code-generator
+  git checkout fd6917601d0a77b6b6df81599118212d8cdb9a27
+  cd ..
+fi
+
+wld="wasm-ld-8 --no-entry --export-dynamic --allow-undefined"
+wcc="clang --target=wasm32 -c -O3"
+$wcc -DNDEBUG -I QR-Code-generator/c QR-Code-generator/c/qrcodegen.c qr.c
+$wld qr.o qrcodegen.o -o qr.wasm
+echo '{"canisters":{"qr":{"main":"src/qr"}}}' > dfx.json
+install -D qr.wasm build/qr/qr.wasm
+head -c 8 /dev/urandom > build/qr/_canister.id
