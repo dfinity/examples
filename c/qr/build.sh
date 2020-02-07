8834aaaf74eaee6c347561c181465f9a740df4f5
Enzo Haussecker <enzo@sovereign.io>
2020-02-07 12:28:49 -0800

    Add QR code generator as submodule

--- c/qr/build.sh
+++ c/qr/build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 set -e
-
-if ! [ -a QR-Code-generator]; then
-  git clone https://github.com/nayuki/QR-Code-generator
-  cd QR-Code-generator
-  git checkout fd6917601d0a77b6b6df81599118212d8cdb9a27
-  cd ..
-fi
 wld="wasm-ld-8 --no-entry --export-dynamic --allow-undefined"
 wcc="clang-8 --target=wasm32 -c -O3"
 $wcc -DNDEBUG -I QR-Code-generator/c QR-Code-generator/c/qrcodegen.c qr.c
