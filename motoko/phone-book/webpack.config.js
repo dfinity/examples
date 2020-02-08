257aecc3a7127d2cf1d73ccc1ab326862ccc2306
Enzo Haussecker <enzo@sovereign.io>
2020-02-07 16:22:41 -0800

    Fix-up webpack config file

--- motoko/phone-book/webpack.config.js
+++ motoko/phone-book/webpack.config.js
@@ -1,16 +1,33 @@
+const dfxJson = require("./dfx.json");
+const fs = require('fs');
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
-const dfxJson = require("./dfx.json");
 
-const outputBase = ["defaults", "build", "output"].reduce(function (accum, x) {
-  accum ? accum[x] : null
+// Identify dfx output directory.
+const output = ["defaults", "build", "output"].reduce(function (accum, x) {
+  return accum && accum[x] ? accum[x] : null
 }, dfxJson) || "build";
+console.log("dfx output directory = " + output);
+
+// Identify dfx version.
+const versions = fs.readdirSync(
+  path.join(process.env["HOME"], ".cache/dfinity/versions")
+);
+const custom = process.env["DFX_VERSION"];
+const latest = versions.map(function (version) {
+  const chunks = version.split('-');
+  const prefix = chunks[0].split('.').map(s => parseInt(s));
+  const suffix = chunks[1] == null ? 0 : parseInt(chunks[1]);
+  return [prefix.concat(suffix).map(n => 1000000 + n).join(), version];
+}).sort().slice(-1)[0][1];
+const version = custom ? custom : latest;
+console.log("dfx version = " + version);
 
 // List of all aliases for canisters. This creates the module alias for
 // the `import ... from "ic:canisters/xyz"` where xyz is the name of a
 // canister.
 const aliases = Object.entries(dfxJson.canisters).reduce((acc, [name,]) => {
-  const outputRoot = path.join(__dirname, outputBase, name);
+  const outputRoot = path.join(__dirname, output, name);
   return {
     ...acc,
     ["ic:canisters/" + name]: path.join(outputRoot, "main.js"),
@@ -21,7 +38,7 @@ const aliases = Object.entries(dfxJson.canisters).reduce((acc, [name,]) => {
   "ic:userlib": path.join(
     process.env["HOME"],
     ".cache/dfinity/versions",
-    dfxJson.dfx || process.env["DFX_VERSION"],
+    version,
     "js-user-library/dist/lib.prod.js",
   ),
 });
@@ -33,7 +50,7 @@ function generateWebpackConfigForCanister(name, info) {
   if (typeof info.frontend !== 'object') {
     return;
   }
-  const outputRoot = path.join(__dirname, outputBase, name);
+  const outputRoot = path.join(__dirname, output, name);
   const inputRoot = __dirname;
   const entry = path.join(inputRoot, info.frontend.entrypoint);
   return {
