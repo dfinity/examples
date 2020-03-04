11941ddf4e27e02235dbada23d531dd458753920
Lisa Gunn <44206363+lsgunnlsgunn@users.noreply.github.com>
2020-03-04 10:46:26 -0800

    fix import line

--- motoko/favorite_cities/src/favorite_cities/public/index.js
+++ motoko/favorite_cities/src/favorite_cities/public/index.js
@@ -1,5 +1,5 @@
 import favorite_cities from 'ic:canisters/favorite_cities';
 
-hello_location_2_13.greet(window.prompt("Enter your name:")).then(greeting => {
+favorite_cities.greet(window.prompt("Enter your name:")).then(greeting => {
   window.alert(greeting);
 });
