ade8f55c320abc0b2459d570f4daab6009d7a28a
Stanley Jones <stanley.jones@dfinity.org>
2020-04-10 08:14:24 -0700

    Fix: remove /canisters

--- motoko/pubsub/canisters/pub/main.js
+++
@@ -1,5 +0,0 @@
-import actorInterface from "ic:idl/pub";
-
-export default icHttpAgent.makeActorFactory(actorInterface)({
-  canisterId: "ic:01E9E179F89D4C89BF",
-});
--- motoko/pubsub/canisters/sub/main.js
+++
@@ -1,5 +0,0 @@
-import actorInterface from "ic:idl/sub";
-
-export default icHttpAgent.makeActorFactory(actorInterface)({
-  canisterId: "ic:ED0606EC6ED1B1F61E",
-});
