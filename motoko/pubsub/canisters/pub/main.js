be4b49858a9503c4d8215ab3a3d003c163f26642
Stanley Jones <stanley.jones@dfinity.org>
2020-04-10 08:13:57 -0700

    Fix: Simplify dfx.json

---
+++ motoko/pubsub/canisters/pub/main.js
@@ -0,0 +1,5 @@
+import actorInterface from "ic:idl/pub";
+
+export default icHttpAgent.makeActorFactory(actorInterface)({
+  canisterId: "ic:01E9E179F89D4C89BF",
+});
---
+++ motoko/pubsub/canisters/sub/main.js
@@ -0,0 +1,5 @@
+import actorInterface from "ic:idl/sub";
+
+export default icHttpAgent.makeActorFactory(actorInterface)({
+  canisterId: "ic:ED0606EC6ED1B1F61E",
+});
