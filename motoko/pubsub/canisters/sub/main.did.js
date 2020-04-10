0b09458451097ecdaeb6257a9900781553f4d893
Stanley Jones <stanley.jones@dfinity.org>
2020-04-09 17:06:43 -0700

    remove /canisters

--- motoko/pubsub/canisters/sub/main.did.js
+++
@@ -1,6 +0,0 @@
-export default ({ IDL }) => {
- const Counter = IDL.Record({'topic': IDL.Text, 'value': IDL.Nat})
- return IDL.Service({'getCount': IDL.Func([], [IDL.Nat], ['query']),
-  'init': IDL.Func([], [], ['oneway']),
-  'updateCount': IDL.Func([Counter], [], ['oneway'])});
-};
--- motoko/pubsub/canisters/sub/main.js
+++
@@ -1,5 +0,0 @@
-import actorInterface from "ic:idl/sub";
-
-export default icHttpAgent.makeActorFactory(actorInterface)({
-  canisterId: "ic:8FF879AB7FFAD1D16D",
-});
