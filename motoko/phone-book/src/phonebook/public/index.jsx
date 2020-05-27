3a638c848767259e85de3e3968158299730f4404
Andrew Wylde <andrew.wylde@dfinity.org>
2020-05-26 20:33:16 -0700

    feature: upgrade phonebook to 0.5.7 compatible

--- motoko/phone-book/src/phonebook/public/index.jsx
+++ motoko/phone-book/src/phonebook/public/index.jsx
@@ -14,14 +14,14 @@ class Phonebook extends React.Component {
     let desc = document.getElementById("newEntryDesc").value;
     let phone = document.getElementById("newEntryPhone").value;
 
-    phonebook.insert(name, desc, parseInt(phone, 10));
+    phonebook.insert(name, desc, parseInt(phone.replace(/\W+/g,''), 10));
   }
 
   async lookup() {
     let name = document.getElementById("lookupName").value;
     phonebook.lookup(name).then(opt_entry => {
       let [entry] = opt_entry;
-      if (entry === null) {
+      if (entry === null || entry === undefined) {
         entry = { name: "", description: "", phone: ""};
       }
       document.getElementById("newEntryName").value = entry.name;
@@ -37,12 +37,12 @@ class Phonebook extends React.Component {
         <div>
           Insert or update new phonebook entry:
           <table>
-            <tr><td>Name:</td><td><input id="newEntryName"></input></td></tr>
+            <tr><td>Name:</td><td><input required id="newEntryName"></input></td></tr>
             <tr><td>Description:</td><td><input id="newEntryDesc"></input></td></tr>
-            <tr><td>Phone:</td><td><input id="newEntryPhone" type="number"></input></td></tr>
+            <tr><td>Phone:</td><td><input required id="newEntryPhone" type="tel" pattern="[0-9]{10}"></input></td></tr>
           </table>
           <button onClick={() => this.doInsert()}>Insert or Update</button>
-        </div> 
+        </div>
         <div>
           Lookup Name: <input id="lookupName"></input> <button onClick={
             () => this.lookup()
