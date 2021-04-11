932020bd951d32a0093dfb36ed792c211ae63a48
Helder Oliveira <heldrida@gmail.com>
2021-04-11 20:14:47 +0100

    fix: 🐛 No such file or directory (os error 2)
    Previously, when running the example shell scripts an error is thrown
    "No such file or directory (os error 2)".
    
    On dfx 0.6.26 the directory is expected to be `.dfx/local/canisters/reverse`,
    for that reason a mkdir command creates the desired build directory.
    
    The script now also skips build if the directory already already exists,
    preceding the call to the canister reverse service.
    
    On sucessful build, the wasm file is now moved instead of copied,
    and artifacts are cleared from root.

--- c/reverse/start.sh
+++ c/reverse/start.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
-dfx canister create reverse
-cp reverse.wasm .dfx/local/canisters/
-dfx canister install --all
+
+BUILD_DIR=.dfx/local/canisters/reverse/
+
+if [ ! -d "$BUILD_DIR" ]; then
+  dfx canister create reverse
+  mkdir -p $BUILD_DIR
+  mv reverse.wasm $BUILD_DIR
+  rm reverse.o
+  dfx canister install --all
+fi
+
 dfx canister call reverse go '("repaid")'
