03e65085c996069855ec4e83aea400c8e20fe190
Claudio Russo <claudio@dfinity.org>
2021-04-23 22:00:28 +0100

    refactor

--- motoko/superheroes/src/www/components/create.jsx
+++ motoko/superheroes/src/www/components/create.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-import { Actor, HttpAgent } from '@dfinity/agent';
-import { idlFactory as superheroes_idl, canisterId as superheroes_id } from 'dfx-generated/superheroes';
-
-const agent = new HttpAgent();
-const Superheroes = Actor.createActor(superheroes_idl, { agent, canisterId: superheroes_id });
+import Superheroes from './superheroes';
 
 const $ = document.getElementById.bind(document);
 const idl = require('../utilities/idl');
