bcc160a4c62b603fa6690b11b0e9306111d312fb
Claudio Russo <claudio@dfinity.org>
2021-07-15 12:52:19 +0100

    upgrade to 0.7.7 (without modifying webpack.config.js or index.js

--- .github/workflows/provision-linux.sh
+++ .github/workflows/provision-linux.sh
@@ -12,7 +12,7 @@ sudo apt-get install --yes nodejs
 rm install-node.sh
 
 # Install DFINITY SDK.
-version=0.7.2
+version=0.7.7
 wget --output-document install-dfx.sh "https://sdk.dfinity.org/install.sh"
 DFX_VERSION=$version bash install-dfx.sh < <(yes Y)
 rm install-dfx.sh
