28ab6fe342ff9071a7b8d89a4fb40f72e6825724
Matthew Hammer <matthew.hammer@gmail.com>
2021-10-12 12:58:23 -0600

    testing and varying client code.

--- motoko/cert-var/src/cert_var_assets/src/index.js
+++ motoko/cert-var/src/cert_var_assets/src/index.js
@@ -1,5 +1,5 @@
 import { Actor, HttpAgent } from '@dfinity/agent';
-import { Certificate } from '@dfinity/agent';
+import { Certificate, makeNonceTransform } from '@dfinity/agent';
 import { cert_var } from '../../declarations'
 
 document.getElementById("certifyBtn").addEventListener("click", async () => {
@@ -7,7 +7,9 @@ document.getElementById("certifyBtn").addEventListener("click", async () => {
   const _ = await cert_var.set(newVal);
   const resp = await cert_var.get();
 
-  const agent = await Promise.resolve(new HttpAgent({ host: 'http://127.0.0.1:' + port, identity })).then(
+  // to do -- From where to get the port and host, in general?
+  const port = 8000;
+  const agent = await Promise.resolve(new HttpAgent({ host: 'http://127.0.0.1:' + port })).then(
       async agent => {
           await agent.fetchRootKey();
           agent.addTransform(makeNonceTransform());
@@ -16,8 +18,13 @@ document.getElementById("certifyBtn").addEventListener("click", async () => {
   );
   const now = Date.now() / 1000;
 
-  const cert = new Certificate(resp.certificate, agent);
-  expect(await cert.verify()).toBe(true);
+  const cert = new Certificate(resp, agent);
+
+  // Check: Certificate verifies.
+  if(!(await cert.verify())) {
+    document.getElementById("var").innerText = "Verification failed.";
+    return
+  }
 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const te = new TextEncoder();
@@ -32,9 +39,16 @@ document.getElementById("certifyBtn").addEventListener("click", async () => {
   )[0];
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const time = decodedTime / 1e9;
-  // The diff between decoded time and local time is within 5s
-  expect(Math.abs(time - now) < 5).toBe(true);
 
+  // Check: The diff between decoded time and local time is within 5s.
+  if(Math.abs(time - now) > 5) {
+    document.getElementById("var").innerText = "Timing is wrong.";
+    return
+  };
+
+  // Checks:
+  // - Canister ID is correct.
+  // - Certified data is correct.
   const pathData = [te.encode('canister'),
                     cert_var.toText(),
                     te.encode('certified_data')];
