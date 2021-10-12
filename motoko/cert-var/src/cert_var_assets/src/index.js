03f924025e1d3e2d991f3855bfc71b22cd3b201f
Yan Chen <yan.chen@dfinity.org>
2021-10-12 15:43:33 -0700

    fix

--- motoko/cert-var/src/cert_var_assets/src/index.js
+++ motoko/cert-var/src/cert_var_assets/src/index.js
@@ -1,41 +1,53 @@
-import { Actor, HttpAgent } from '@dfinity/agent';
-import { Certificate, makeNonceTransform } from '@dfinity/agent';
-import { cert_var } from '../../declarations'
+import { HttpAgent } from '@dfinity/agent';
+import { Certificate } from '@dfinity/agent';
+import { IDL } from '@dfinity/candid';
+import { Principal } from '@dfinity/principal'
+import { cert_var, canisterId } from '../../declarations'
+
+const agent = new HttpAgent({});
+const hostname = agent._host.hostname;
+if (process.env.NODE_ENV !== "production") {
+  agent.fetchRootKey();
+}
+
+function hexToBytes(hex) {
+    for (var bytes = [], c = 0; c < hex.length; c += 2)
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+    return bytes;
+}
 
 document.getElementById("certifyBtn").addEventListener("click", async () => {
   const newVal = BigInt(document.getElementById("newValue").value);
-  const _ = await cert_var.set(newVal);
+  await cert_var.set(newVal);
   const resp = await cert_var.get();
 
-  // to do -- From where to get the port and host, in general?
-  const port = 8000;
-  const agent = await Promise.resolve(new HttpAgent({ host: 'http://127.0.0.1:' + port })).then(
-      async agent => {
-          await agent.fetchRootKey();
-          agent.addTransform(makeNonceTransform());
-          return agent;
-      },
-  );
-  const cert = new Certificate(resp.certificate[0], agent);
+  const log = document.getElementById("var");
+  log.innerText = "Verifying...";
+
+  const readState = { certificate: new Uint8Array(resp.certificate[0]) };
+  const cert = new Certificate(readState, agent);
 
   // Check: Certificate verifies.
   if(!(await cert.verify())) {
-    document.getElementById("var").innerText = "Verification failed.";
-    return
+    log.innerText = "Verification failed.";
+    return;
   }
-
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  
   const te = new TextEncoder();
   const pathTime = [te.encode('time')];
   const rawTime = cert.lookup(pathTime);
-  const decodedTime = IDL.decode(
-    [IDL.Nat],
-    new Uint8Array([
+  console.log(rawTime);
+  /*
+  const idlMessage = new Uint8Array([
       ...new TextEncoder().encode('DIDL\x00\x01\x7d'),
       ...(new Uint8Array(rawTime) || []),
-    ]),
+  ]);
+  console.log(idlMessage);
+  const decodedTime = IDL.decode(
+    [IDL.Nat], idlMessage
   )[0];
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  console.log(decodedTime);
+
   const time = decodedTime / 1e9;
 
   // Check: The diff between decoded time and local time is within 5s.
@@ -43,24 +55,27 @@ document.getElementById("certifyBtn").addEventListener("click", async () => {
   if(Math.abs(time - now) > 5) {
     document.getElementById("var").innerText = "Timing is wrong.";
     return
-  };
+  };*/
 
   // Checks:
   // - Canister ID is correct.
   // - Certified data is correct.
+  const cid = new Uint8Array(hexToBytes(Principal.fromText(canisterId).toHex()));
   const pathData = [te.encode('canister'),
-                    cert_var.toText(),
+                    cid,
                     te.encode('certified_data')];
   const rawData = cert.lookup(pathData);
+  console.log(rawData);
+  /*
   const decodedData = IDL.decode(
     [IDL.Nat32],
     new Uint8Array([
       ...new TextEncoder().encode('DIDL\x00\x01\x7d'),
-      ...(new Uint8Array(rawData) || []),s
+      ...(new Uint8Array(rawData) || []),
     ]),
   )[0];
   const expectedData = IDL.Nat32.encodeValue(resp.value);
-  expect(decodedData == expectedData).toBe(true);
+  expect(decodedData == expectedData).toBe(true);*/
 
-  document.getElementById("var").innerText = "Certified response.";
+  log.innerText = "Certified response.";
 });
