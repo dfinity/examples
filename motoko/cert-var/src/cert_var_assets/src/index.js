eaf0b2f232f8d736e2b2164a5ee89448d6fab08e
Matthew Hammer <matthew.hammer@gmail.com>
2021-10-21 13:02:38 -0600

    document with a long comment

--- motoko/cert-var/src/cert_var_assets/src/index.js
+++ motoko/cert-var/src/cert_var_assets/src/index.js
@@ -25,15 +25,67 @@ document.getElementById("setBtn").addEventListener("click", async () => {
 
   log.innerText = "Verifying gotten value " + newVal + "from " + cid + "...";
 
+  /*
+    # Response certification example: Single 32-bit Variable.
+
+    To detect an attacker in the middle between us and the IC (and our
+    "true" canister running there), we must either:
+
+    - perform update calls that use "full consensus" (and wait for ~2 sec).
+    - perform (fast) query calls whose responses that we, the client, certify,
+      using the coordination of the IC and our canister running there.
+
+    This code demonstrates the second approach here, in a minimal setting.
+
+    The full example (beyond this file) consists of a Motoko canister
+    (called cert_var here) that holds a single certified variable, as
+    a 32-bit number, and an asset canister whose (client-side) code
+    (here!) queries and certifies this number's "current certificate".
+
+    The Motoko backend canister prepares for our certification here by
+    giving us a "current certificate" within the response; this certificate
+    is signed by the entire IC, using a system feature described here:
+
+    https://sdk.dfinity.org/docs/interface-spec/index.html#system-api-certified-data
+
+    Before we trust the response from our apparently "true" canister,
+    we interrogate it, and verify its authenticity:
+
+    We do four checks below:
+
+    1. verify system certificate.
+    2. check system certificate timestamp is not "too old".
+    3. check canister ID in system certificate.
+    4. check response matches witness.
+
+    For steps 2, 3 and 4, we access data from the certificate (Blob).
+    The Certificate class from the agent-js library provides a way to
+    access those items using their paths, like a filesystem, each addressing
+    a Blob, encoding something.
+
+    In the case of time and our data, the encodings are each Candid.
+    Time is always a Candid Nat, and our data we choose to encode the same as
+    a Candid 32-bit Nat (see the Motoko canister for details).
+
+    Notably, in an example with more data in the canister than a single number,
+    or a more complex query interface, we would generally do more work to
+    certify each query response:
+
+    5. use witnesss to re-calculate hash (no witness or hashing needed here.)
+    6. check query parameters matches witness (no params, so trivial here.)
+
+    Neither of those steps are needed here, for the reasons given above.
+  */
+
   const readState = { certificate: new Uint8Array(resp.certificate[0]) };
   const cert = new Certificate(readState, agent);
 
-  // Check: Certificate verifies.
+  // Check 1: Certificate verifies.
   if(!(await cert.verify())) {
     log.innerText = "Failure: Certification verification failed.";
     return;
   }
-  
+
   const te = new TextEncoder();
   const pathTime = [te.encode('time')];
   const rawTime = cert.lookup(pathTime);
@@ -46,14 +98,14 @@ document.getElementById("setBtn").addEventListener("click", async () => {
   )[0];
   const time = Number(decodedTime) / 1e9;
 
-  // Check: The diff between decoded time and local time is within 5s.
+  // Check 2: The diff between decoded time and local time is within 5s.
   const now = Date.now() / 1000;
   if(Math.abs(time - now) > 5) {
     document.getElementById("var").innerText = "Failure: Timing is wrong.";
     return;
   };
 
-  // Checks:
+  // Checks 3 and 4:
   // - Canister ID is correct.
   // - Certified data is correct.
   const pathData = [te.encode('canister'),
