db7b9aef8d4ec2ea4106218b85be34f626e00511
Claudio Russo <claudio@dfinity.org>
2021-12-17 19:45:29 +0000

    upgrade to dfx 0.8.4 (#129)
    * upgrade to dfx 0.8.4
    
    * npm audit fix --force
    
    * npm audit fix --force
    
    * npm audit fix --force; simplify readme
    
    * npm audit fix --force

--- motoko/phone-book/src/declarations/index.js
+++ motoko/phone-book/src/declarations/index.js
@@ -1,13 +1,14 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
 
-// Imports candid interface
+// Imports and re-exports candid interface
 import { idlFactory } from './phone_book.did.js';
-// CANISTER_ID is replaced by webpack based on node enviroment
+export { idlFactory } from './phone_book.did.js';
+// CANISTER_ID is replaced by webpack based on node environment
 export const canisterId = process.env.PHONE_BOOK_CANISTER_ID;
 
 /**
  * 
- * @param {string | Principal} canisterId Canister ID of Agent
+ * @param {string | import("@dfinity/principal").Principal} canisterId Canister ID of Agent
  * @param {{agentOptions?: import("@dfinity/agent").HttpAgentOptions; actorOptions?: import("@dfinity/agent").ActorConfig}} [options]
  * @return {import("@dfinity/agent").ActorSubclass<import("./phone_book.did.js")._SERVICE>}
  */
@@ -15,7 +16,12 @@ export const canisterId = process.env.PHONE_BOOK_CANISTER_ID;
   const agent = new HttpAgent({ ...options?.agentOptions });
   
   // Fetch root key for certificate validation during development
-  if(process.env.NODE_ENV !== "production") agent.fetchRootKey();
+  if(process.env.NODE_ENV !== "production") {
+    agent.fetchRootKey().catch(err=>{
+      console.warn("Unable to fetch root key. Check to ensure that your local replica is running");
+      console.error(err);
+    });
+  }
 
   // Creates an actor with using the candid interface and the HttpAgent
   return Actor.createActor(idlFactory, {
