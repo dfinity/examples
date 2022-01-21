030fd29bf65599449212af9d70eedb1b46e09047
John Plevyak <jplevyak@gmail.com>
2022-01-21 08:05:20 -0800

    Add ledger did and wasm.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

---
+++ rust/defi/install-local-ledger.sh
@@ -0,0 +1,19 @@
+dfx stop
+dfx start --background --clean
+
+dfx identity new minter
+dfx identity use minter
+export MINT_ACC=$(dfx ledger account-id)
+
+dfx identity use default
+export LEDGER_ACC=$(dfx ledger account-id)
+
+# Use private api for install
+rm src/ledger/ledger.did
+cp src/ledger/ledger.private.did src/ledger/ledger.did
+
+dfx deploy ledger --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})'
+
+# Replace with public api
+rm src/ledger/ledger.did
+cp src/ledger/ledger.public.did src/ledger/ledger.did
