f9bc791afeccb83792e235a5362b333e5288b850
Kyle Peacock <kylpeacock@gmail.com>
2022-01-25 10:22:36 -0800

    chore:github automation

--- .github/workflows/provision-darwin.sh
+++ .github/workflows/provision-darwin.sh
@@ -22,5 +22,20 @@ curl --location --output install-dfx.sh "https://sdk.dfinity.org/install.sh"
 DFX_VERSION=$version bash install-dfx.sh < <(yes Y)
 rm install-dfx.sh
 
+# Install Moc
+curl --location --output motoko-macos.tar.gz "https://github.com/dfinity/motoko/releases/download/0.6.20/motoko-macos-0.6.20.tar.gz"
+gunzip motoko-macos.tar.gz
+tar -xvf motoko-macos.tar
+rm motoko-macos.tar.gz
+rm motoko-macos.tar
+
+# Install Vessel and Alias Moc
+curl --location --output vessel-macos "https://github.com/dfinity/vessel/releases/download/v0.6.2/vessel-macos"
+chown -R "$(whoami)" ./vessel-macos && chmod -R +x ./vessel-macos
+alias vessel=$(pwd)/vessel-macos
+alias moc=$(vessel bin)/moc
+alias mo-doc=$(vessel bin)/mo-doc
+alias mo-ide=$(vessel bin)/mo-ide
+
 # Exit temporary directory.
 popd
--- .github/workflows/provision-linux.sh
+++ .github/workflows/provision-linux.sh
@@ -17,6 +17,14 @@ wget --output-document install-dfx.sh "https://sdk.dfinity.org/install.sh"
 DFX_VERSION=$version bash install-dfx.sh < <(yes Y)
 rm install-dfx.sh
 
+# Install Vessel and Alias Moc
+curl --location --output vessel-linux64 "https://github.com/dfinity/vessel/releases/download/v0.6.2/vessel-linux64"
+chown -R "$(whoami)" ./vessel-linux64 && chmod -R +x ./vessel-linux64
+alias vessel=$(pwd)/vessel-linux64
+alias moc=$(vessel bin)/moc
+alias mo-doc=$(vessel bin)/mo-doc
+alias mo-ide=$(vessel bin)/mo-ide
+
 # Set environment variables.
 echo "$HOME/bin" >> $GITHUB_PATH
 
