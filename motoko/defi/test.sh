371a01feec55b965ff275df910681db8285d8d91
John Plevyak <jplevyak@gmail.com>
2022-02-13 22:31:20 -0800

    Update motoko ./test.sh to use the grep test for success and to use 'clear' and 'credit' for setup.

--- motoko/defi/test.sh
+++ motoko/defi/test.sh
@@ -1,11 +1,19 @@
-#!/bin/bash
-echo "~~~ Running DeFi test script ~~~"
+# set -x
+set -e
+trap 'catch' ERR
+catch() {
+  dfx identity use default
+  echo "FAIL"
+  exit 1
+}
 
 # Canister principals
 DEX=$(dfx canister id defi_dapp)
 ICP=$(dfx canister id ledger)
 AKI=$(dfx canister id AkitaDIP20)
 GLD=$(dfx canister id GoldenDIP20)
+WALLET=$(dfx identity get-wallet)
+PRINCIPAL=$(dfx identity get-principal)
 
 #dfx canister call defi_dapp placeOrder '(principal '\"$(dfx canister id ledger)\"', 10, principal '\"$(dfx canister id GoldenDIP20)\"', 130)'
 #dfx canister call defi_dapp placeOrder '(principal '\"$(dfx canister id ledger)\"', 10, principal '\"$(dfx canister id AkitaDIP20)\"', 130)'
@@ -17,6 +25,10 @@ function place_order {
 #echo "Cancelling all standing orders..."
 #dfx canister call defi_dapp cancel_all_orders
 # => re-install instead: dfx canister install defi_dapp --mode reinstall
+dfx canister --wallet "${WALLET}" call defi_dapp clear
+dfx canister --wallet "${WALLET}" call defi_dapp credit "(principal \"${PRINCIPAL}\", principal \"${AKI}\", 1000000: nat)"
+dfx canister --wallet "${WALLET}" call defi_dapp credit "(principal \"${PRINCIPAL}\", principal \"${GLD}\", 1000000: nat)"
+dfx canister --wallet "${WALLET}" call defi_dapp credit "(principal \"${PRINCIPAL}\", principal \"${ICP}\", 1000000: nat)"
 
 # Fill book with orders
 # Bid
@@ -36,3 +48,9 @@ place_order $GLD 25000 $ICP 2500
 
 # partial order execution while buying
 place_order $ICP 12000 $GLD 20000
+
+dfx canister call defi_dapp getOrders
+dfx canister call defi_dapp getOrders | grep "fromAmount = 3_000"
+dfx canister call defi_dapp getBalances
+dfx canister call defi_dapp getBalances | grep "amount = 975_400"
+echo "PASS"
