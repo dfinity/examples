36a0aa1a9a8aba24942b6731f61dd81ee980b5dc
Nikolay Komarevskiy <nikolay.komarevskiy@dfinity.org>
2022-02-24 20:09:12 +0100

    WIP

--- motoko/encrypted-notes-dapp/test_whoami.sh
+++ motoko/encrypted-notes-dapp/test_whoami.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-if [ $(dfx canister call encrypted_notes_motoko whoami | grep -e "blabla") ]; then 
+if [ $(dfx canister call encrypted_notes_motoko whoami | grep -e "$(dfx identity get-principal)") ]; then 
     echo "Test passed!"; 
 else
     echo "Test failed :-("
