c977ffd528fa22507093f948fd575845767958b9
Yvonne-Anne Pignolet <yap@dfinity.org>
2022-03-14 15:30:50 +0000

    read: ./pre_deploy.sh: 15: read: Illegal option -d

--- motoko/encrypted-notes-dapp/pre_deploy.sh
+++ motoko/encrypted-notes-dapp/pre_deploy.sh
@@ -12,13 +12,13 @@ sed "s/{{ BUILD_ENV }}/${BUILD_ENV}/" dfx.json.template > dfx.json
 echo "--- Generated dfx.json ---"
 
 if [ "$BUILD_ENV" = "motoko" ]; then 
-read -r -d '' BACKEND_TS <<- EOM
+read -r BACKEND_TS <<- EOM
 export { idlFactory } from './idlFactory';
 export * from '../../../declarations/encrypted_notes_motoko/encrypted_notes_motoko.did.js';
 export const ENCRYPTED_NOTES_CANISTER_ID = process.env.ENCRYPTED_NOTES_MOTOKO_CANISTER_ID;
 EOM
 else 
-read -r -d '' BACKEND_TS <<- EOM
+read -r BACKEND_TS <<- EOM
 export { idlFactory } from './idlFactory';
 export type { _SERVICE } from '../../../declarations/encrypted_notes_rust/encrypted_notes_rust.did.js';
 export const ENCRYPTED_NOTES_CANISTER_ID = process.env.ENCRYPTED_NOTES_RUST_CANISTER_ID;
@@ -28,12 +28,12 @@ echo "$BACKEND_TS" > "$BACKEND_HEADER_FILE"
 echo "--- Generated $BACKEND_HEADER_FILE ---"
 
 if [ "$BUILD_ENV" = "motoko" ]; then 
-read -r -d '' IDL_FACTORY_TS <<- EOM
+read -r IDL_FACTORY_TS <<- EOM
 // d.ts file is broken, importing idlFactory doesn't work from a .ts file
 export { idlFactory } from '../../../declarations/encrypted_notes_motoko/encrypted_notes_motoko.did.js';
 EOM
 else 
-read -r -d '' IDL_FACTORY_TS <<- EOM
+read -r IDL_FACTORY_TS <<- EOM
 // d.ts file is broken, importing idlFactory doesn't work from a .ts file
 export { idlFactory } from '../../../declarations/encrypted_notes_rust/encrypted_notes_rust.did.js';
 EOM
