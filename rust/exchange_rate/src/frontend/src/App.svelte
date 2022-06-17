ee9491e77eac0f46259201f4ebbf6b257ef67e62
Ruediger Birkner <ruediger.birkner@dfinity.org>
2022-06-17 16:37:07 +0200

    frontend canister (WIP)

---
+++ rust/exchange_rate/src/frontend/src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Chart from "./components/Chart.svelte";
+</script>
+
+<main>
+  <h1>ICP Price Chart</h1>
+
+  <Chart />
+
+  <a
+    href="https://dfinity.org"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="logo"
+  >
+    <img src="images/ic-badge-powered-by-crypto_label-stripe-white-text.png" alt="DFINITY logo" />
+  </a>
+</main>
+
+<style>
+  h1 {
+    color: #a9a9a9;
+    font-weight: 700;
+  }
+
+  .logo {
+    display: inline-block;
+    margin-bottom: 64px;
+    margin-top: 64px;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: 800px;
+      margin: 0 auto;
+    }
+  }
+</style>
