2e34a1b9a2ef6693c265960b874a76164959509c
QJ <qijing.yu@dfinity.org>
2022-06-20 00:25:00 +0000

    return rates with sample interval. backend <-> frontend integration.

--- rust/exchange_rate/src/frontend/src/components/Chart.svelte
+++ rust/exchange_rate/src/frontend/src/components/Chart.svelte
@@ -1,27 +1,27 @@
 <script>
   // agent to connect to backend canister
-  import { backend_dummy } from "../../../declarations/backend_dummy/index.js";
+  import { exchange_rate } from "../../../declarations/exchange_rate/index.js";
 
   // loading spinner
   import OverlayLoading from "svelte-overlay-loading";
   import { Stretch } from "svelte-loading-spinners";
 
-	import chartjs from 'chart.js/auto'
-	import { onMount } from 'svelte';
+  import chartjs from "chart.js/auto";
+  import { onMount } from "svelte";
 
   // date picker
-	import Flatpickr from 'svelte-flatpickr';
-	import 'flatpickr/dist/flatpickr.css';
+  import Flatpickr from "svelte-flatpickr";
+  import "flatpickr/dist/flatpickr.css";
 
   // icons
-  import Fa from 'svelte-fa'
-  import { faSync } from '@fortawesome/free-solid-svg-icons'
-  import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
+  import Fa from "svelte-fa";
+  import { faSync } from "@fortawesome/free-solid-svg-icons";
+  import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 
-	let ctx;
+  let ctx;
   let chart;
 
-	let startTime = 1654034400;
+  let startTime = 1654034400;
   let endTime = 1654812000;
 
   let loading = true;
@@ -44,8 +44,10 @@
     var increment = 3600;
     // TO REMOVE
 
-    const rates = await backend_dummy.get_rates(start, end);
-    var sortedRates = rates.sort(function(a, b) { return Number(a[0]) - Number(b[0]) })
+    const rates = await exchange_rate.get_rates(start, end);
+    var sortedRates = rates.sort(function (a, b) {
+      return Number(a[0]) - Number(b[0]);
+    });
     var next = start;
     sortedRates.forEach(function (item, index) {
       var xValue = Number(item[0]);
@@ -72,59 +74,62 @@
     chart.update(); // notify chart.js to render the new data
   }
 
-	onMount(async () => {
+  onMount(async () => {
     getExchangeRates(startTime, endTime);
 
-			chart = new chartjs(ctx, {
-				type: 'line',
-				data: {
-						labels: xValues,
-						datasets: [{
-								borderColor: '#d3d3d3',
-								data: yValues
-						}]
-				},
-        options: {
-          animation: {
-            duration: 0
+    chart = new chartjs(ctx, {
+      type: "line",
+      data: {
+        labels: xValues,
+        datasets: [
+          {
+            borderColor: "#d3d3d3",
+            data: yValues,
           },
-          plugins: {
-            legend: {
-              display: false
-            },
-            tooltip: {
-              enabled: true,
-              displayColors: false,
-              callbacks: {
-                title: function(context) {
-                  return timestampToString(parseInt(context[0].label));
-                },
-                label: function(context) {
-                  return "$" + Number(context.parsed.y).toFixed(2);
-                },
-              }
-            },
+        ],
+      },
+      options: {
+        animation: {
+          duration: 0,
+        },
+        plugins: {
+          legend: {
+            display: false,
           },
-          scales: {
-            y: {
-              ticks: {
-                callback: function(value, index, ticks) {
-                  return '$' + Number(value).toFixed(2);
-                }
-              }
-            },
-            x: {
-              ticks: {
-                callback: function(val, index) {
-                  return index % 2 === 0 ? timestampToString(this.getLabelForValue(val)) : '';
-                },
+          tooltip: {
+            enabled: true,
+            displayColors: false,
+            callbacks: {
+              title: function (context) {
+                return timestampToString(parseInt(context[0].label));
+              },
+              label: function (context) {
+                return "$" + Number(context.parsed.y).toFixed(2);
               },
             },
-          }
-        }
-		});
-
-	});
+          },
+        },
+        scales: {
+          y: {
+            ticks: {
+              callback: function (value, index, ticks) {
+                return "$" + Number(value).toFixed(2);
+              },
+            },
+          },
+          x: {
+            ticks: {
+              callback: function (val, index) {
+                return index % 2 === 0
+                  ? timestampToString(this.getLabelForValue(val))
+                  : "";
+              },
+            },
+          },
+        },
+      },
+    });
+  });
 
   function timestampToString(timestamp) {
     var date = new Date(timestamp * 1000);
@@ -135,62 +140,75 @@
     var minutes = "0" + date.getMinutes();
     var seconds = "0" + date.getSeconds();
 
-    return year + "-" + month + "-" + day + " " + hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+    return (
+      year +
+      "-" +
+      month +
+      "-" +
+      day +
+      " " +
+      hours +
+      ":" +
+      minutes.substr(-2) +
+      ":" +
+      seconds.substr(-2)
+    );
   }
 
   // configuration of date picker (flatpickr)
-	const options = {
-		enableTime: true,
+  const options = {
+    enableTime: true,
     time_24hr: true,
     mode: "range",
     minDate: "2021-05-11",
     maxDate: new Date(),
     defaultDate: [new Date().fp_incr(-7), new Date().fp_incr(-1)],
-		onChange(selectedDates, dateStr) {
+    onChange(selectedDates, dateStr) {
       if (selectedDates.length == 2) {
         startTime = Math.floor(selectedDates[0].getTime() / 1000);
         endTime = Math.floor(selectedDates[1].getTime() / 1000);
       }
 
-      console.log('startTime: ', startTime);
-      console.log('endTime: ', endTime);
-		}
-	};
+      console.log("startTime: ", startTime);
+      console.log("endTime: ", endTime);
+    },
+  };
 
-	function handleSubmit(event) {
-		event.preventDefault();
-		getExchangeRates(startTime, endTime);
-	}
+  function handleSubmit(event) {
+    event.preventDefault();
+    getExchangeRates(startTime, endTime);
+  }
 </script>
 
-<canvas bind:this={ctx} id="myChart"></canvas>
+<canvas bind:this={ctx} id="myChart" />
 
 {#if missingData}
-<div class="warning">
-  <Fa icon={faTriangleExclamation} /> Parts of the data are missing and are still being fetched. Retry a bit later...
-</div>
+  <div class="warning">
+    <Fa icon={faTriangleExclamation} /> Parts of the data are missing and are still
+    being fetched. Retry a bit later...
+  </div>
 {/if}
 
 <div class="date-picker">
-	<form on:submit={handleSubmit}>
-		<Flatpickr {options} name="date" element="#date-picker">
+  <form on:submit={handleSubmit}>
+    <Flatpickr {options} name="date" element="#date-picker">
       <div id="date-picker">
         <input type="text" placeholder="Select time range..." data-input />
       </div>
     </Flatpickr>
 
-		<button type="submit">
-			<Fa icon={faSync} /> Update
-		</button>
-	</form>
+    <button type="submit">
+      <Fa icon={faSync} /> Update
+    </button>
+  </form>
 </div>
 
 {#if loading}
-<OverlayLoading>
-  <div class="center">
-    <Stretch color="#d3d3d3" />
-  </div>
-</OverlayLoading>
+  <OverlayLoading>
+    <div class="center">
+      <Stretch color="#d3d3d3" />
+    </div>
+  </OverlayLoading>
 {/if}
 
 <style>
@@ -247,5 +265,3 @@
     font-size: 15px;
   }
 </style>
-
-
---
+++ rust/exchange_rate/src/main.rs
@@ -0,0 +1,321 @@
+use candid::{candid_method, CandidType, Principal};
+use ic_cdk::{self};
+use ic_cdk_macros::{self, heartbeat, query, update};
+use queues::*;
+use serde::{Deserialize, Serialize};
+use std::cell::RefCell;
+use std::collections::HashMap;
+use chrono::{DateTime, NaiveDateTime, Utc};
+
+type Timestamp = u64;
+type Rate = String;
+// type Rate = f32;
+
+#[derive(CandidType, Clone, Deserialize, Debug, Eq, Hash, PartialEq, Serialize)]
+pub struct TimeRange {
+    pub start: Timestamp,
+    pub end: Timestamp,
+}
+
+#[derive(Clone, Debug, PartialEq, CandidType, Eq, Serialize, Deserialize)]
+pub struct RatesWithInterval {
+    pub interval: usize,
+    pub rates: HashMap<Timestamp, Rate>
+}
+
+#[derive(CandidType, Clone, Deserialize, Debug, Eq, Hash, PartialEq, Serialize)]
+pub struct HttpHeader {
+    pub name: String,
+    pub value: String,
+}
+
+#[derive(Clone, Debug, PartialEq, CandidType, Eq, Hash, Serialize, Deserialize)]
+pub enum HttpMethod {
+    GET,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub struct CanisterHttpRequestArgs {
+    pub url: String,
+    pub headers: Vec<HttpHeader>,
+    pub body: Option<Vec<u8>>,
+    pub http_method: HttpMethod,
+    pub transform_method_name: Option<String>,
+}
+
+#[derive(CandidType, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct CanisterHttpResponsePayload {
+    pub status: u64,
+    pub headers: Vec<HttpHeader>,
+    pub body: Vec<u8>,
+}
+
+// How many data point can be returned as maximum. 
+// Given that 2MB is max-allow cansiter response size, and each <Timestamp, Rate> pair
+// should be less that 100 bytes. Maximum data points could be returned for each
+// call can be as many as 2MB / 100B = 20000. 
+pub const MAX_DATA_PONTS_COUNT: usize = 20000;
+
+// Remote fetch interval is always 60 secs. It is only the canister returned interval
+// that is dynamic according to the data size needs to be returned.
+pub const REMOTE_FETCH_INTERVAL: i64 = 60;  
+
+thread_local! {
+    pub static FETCHED: RefCell<HashMap<Timestamp, String>>  = RefCell::new(HashMap::new());
+    pub static REQUESTED: RefCell<Queue<Timestamp>> = RefCell::new(Queue::new());
+    pub static HEARTBEAT_COUNT: RefCell<i32> = RefCell::new(0);
+
+    pub static RESPONSE_HEADERS_SANTIZATION: Vec<&'static str> = vec![
+        "x-mbx-uuid",
+        "x-mbx-used-weight",
+        "x-mbx-used-weight-1m",
+        "Date",
+        "Via",
+        "X-Amz-Cf-Id",
+    ];
+}
+
+// Canister heartbeat. Process one item in queue
+// #[export_name = "canister_heartbeat"]
+#[heartbeat]
+async fn heartbeat() {
+    ic_cdk::api::print(format!("Heartbeat method invoked."));
+
+    // TODO: Remove. Below chunk is only for debugging purpose.
+    // let mut heartbeat_count = 0;
+    // HEARTBEAT_COUNT.with(|count_lock| {
+    //     let mut count = count_lock.borrow_mut();
+    //     *count += 1;
+    //     heartbeat_count = *count;
+    // });
+    // FETCHED.with(|fetched_lock| {
+    //     let mut stored = fetched_lock.borrow_mut();
+    //     let message = format!("Heartbeat invoked {} times.", heartbeat_count);
+    //     stored.insert(27540860, message);
+    // });
+
+    get_next_rate().await;
+}
+
+/*
+Get rates for a time range defined by start time and end time. This function can be invoked
+as HTTP update call.
+*/
+#[update]
+#[candid_method(update)]
+async fn get_rates(range: TimeRange) -> RatesWithInterval {
+    // round down start time and end time to the minute (chop off seconds), to be checked in the hashmap
+    ic_cdk::api::print(format!("get_rates method invoked."));
+
+    // normalize the start_time and end_time to the minute before query remote.
+    let start_min = range.start / 60;
+    let end_min = range.end / 60;
+
+    // compose a return structure
+    let mut fetched = HashMap::new();
+
+    // pull available ranges from hashmap
+    FETCHED.with(|map_lock| {
+        let map = map_lock.borrow();
+        ic_cdk::api::print("In Fetched.");
+        for requested_min in start_min..end_min {
+                ic_cdk::api::print("In iterations");
+
+            let requested = requested_min * 60;
+            if map.contains_key(&requested) {
+                // The fetched slot is within user requested range. Add to result for later returning.
+                ic_cdk::api::print(format!("Found {} in map!", requested));
+                fetched.insert(requested, map.get(&requested).unwrap().clone().to_string());
+            } else {
+                ic_cdk::api::print(format!("Did not find {} in map!", requested));
+                // asynchoronously request downloads for unavailable ranges
+
+                // Simply putting the request to request queue. This queue will likely
+                // have duplicate entries, if users request same range of data multiple times.
+                // Double downloading is avoided right before the time of downloading by checking
+                // whether the data already exists in FETCHED map.
+                add_job_to_queue(requested);
+            }
+        }
+    });
+
+    // return sampled rates for available ranges
+    sample_with_interval(fetched)
+}
+
+fn sample_with_interval(fetched: HashMap<Timestamp, String>) -> RatesWithInterval {
+    // in order to make sure that returned data do not exceed 2MB, which is about
+    // ~1M data points, calculate interval when data points count is beyond 900K.
+    let interval_options = vec![
+        1,         // 1 data point every minute
+        5,         // 1 data point every 5 minutes
+        15,        // 1 data point every 15 minutes
+        60,        // 1 data point every hour
+        60 * 12,   // 1 data point every 12 hours
+        60 * 24    // 1 data point every day
+    ];
+    for i in interval_options {
+        if fetched.len() / i < MAX_DATA_PONTS_COUNT {
+            return RatesWithInterval {
+                interval: i * 60,
+                rates: fetched.into_iter().filter(|r| r.0 % (i as u64) == 0).collect()
+            };
+        }
+    }
+    panic!("This shouldn't be happening! Couldn't find a inteval that can keep total data points count in {}.", MAX_DATA_PONTS_COUNT);
+}
+
+fn add_job_to_queue(job: Timestamp) -> () {
+    REQUESTED.with(|requested_lock| {
+        let mut queue = requested_lock.borrow_mut();
+        match queue.add(job) {
+            Ok(_) => {
+                ic_cdk::api::print(format!("Added {} to queue.", job));
+            }
+            Err(failure) => {
+                ic_cdk::api::print(format!("Wasn't able to add job {} to queue. Receiving error {}", job, failure));
+            }
+        }
+    });
+}
+
+/*
+Register the cron job which take the tip of the queue, and send a canister call to self.
+Potentially, different nodes executing the canister will trigger different job during the same period.
+The idea is to gap the cron job with large enough time gap, so they won't trigger remove service side
+rate limiting.
+ */
+#[update]
+#[candid_method(update)]
+async fn get_next_rate() -> Option<String> {
+    let mut job_id: u64 = 0;
+
+    // Get the next downloading job
+    REQUESTED.with(|requested_lock| {
+        let mut requested = requested_lock.borrow_mut();
+
+        if requested.size() == 0 {
+            ic_cdk::api::print("Request queue empty, no more jobs to fetch.");
+            return;
+        }
+
+        let job = requested.remove();
+        match job {
+            Ok(valid_job) => {
+                // Job is a valid Job Id
+                job_id = valid_job;
+                
+                FETCHED.with(|fetched_lock| {
+                    match fetched_lock.borrow().get(&valid_job) {
+                        Some(_) => {
+                            // If this job has already been downloaded. Only downloading it if doesn't already exist.
+                            ic_cdk::api::print(format!("Rate for {} is already downloaded. Skipping downloading again.", valid_job));
+                            return;
+                        }
+                        None => {
+                            // The requested time rate isn't found in map. Send a canister get_rate call to self
+                            ic_cdk::api::print(format!("Fetching job {} now.", valid_job));
+                        }
+                    }
+                });
+            }
+            Err(weird_job) => {
+                ic_cdk::api::print(format!("Invalid job found in the request queue! The job Id should be a Unix timestamp divided by 60, e.g., represents a timestamp rounded to minute. Wrong Job Id: {}", weird_job));
+                return;
+            }
+        }
+    });
+    if job_id != 0 {
+        let rate = get_rate(job_id).await;
+        return rate;
+    }
+    return None;
+}
+
+/*
+A function to call IC http_request function with a single minute range.
+This function is to be triggered by timer as jobs move to the tip of the queue.
+ */
+async fn get_rate(job: Timestamp) -> Option<String> {
+    let start_timestamp = (job * 60) as i64;
+    let end_timestamp = ((job + 1) * 60 - 1) as i64;
+
+    let start_time = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(start_timestamp, 0), Utc);
+    let end_time = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(end_timestamp, 0), Utc);
+
+    // prepare system http_request call
+    let mut request_headers = vec![];
+    request_headers.insert(
+        0,
+        HttpHeader {
+            name: "Connection".to_string(),
+            value: "keep-alive".to_string(),
+        },
+    );
+
+    let request = CanisterHttpRequestArgs {
+        url: format!("https://api.pro.coinbase.com/products/ICP-USD/candles?granularity=60&start={start_time}&end={end_time}"),
+        http_method: HttpMethod::GET,
+        body: None,
+        transform_method_name: Some("transform".to_string()),
+        headers: request_headers,
+    };
+
+    let body = candid::utils::encode_one(&request).unwrap();
+    ic_cdk::api::print(format!("Making IC http_request call {} now.", job));
+
+    match ic_cdk::api::call::call_raw(
+        Principal::management_canister(),
+        "http_request",
+        &body[..],
+        0,
+    )
+    .await
+    {
+        Ok(result) => {
+            ic_cdk::api::print("In success path.");
+            // decode the result
+            let decoded_result: String = candid::utils::decode_one(&result).unwrap();
+            ic_cdk::api::print(format!("Got decoded result: {}", decoded_result));
+            // put the result to hashmap
+            FETCHED.with(|fetched_lock| {
+                let mut stored = fetched_lock.borrow_mut();
+                stored.insert(job, decoded_result.clone());
+            });
+            return Some(decoded_result);
+        }
+        Err((r, m)) => {
+            let message =
+                format!("The http_request resulted into error. RejectionCode: {r:?}, Error: {m}");
+            ic_cdk::api::print(message.clone());
+            // TODO - Remove this. Putting the result to hashmap for debuging purpose
+            FETCHED.with(|fetched_lock| {
+                let mut stored = fetched_lock.borrow_mut();
+                stored.insert(job, message.clone());
+            });
+            
+            // Since the remote request failed. Adding the de-queued job back again for retries.
+            add_job_to_queue(job);
+            return Some(message);
+        }
+    }
+}
+
+#[query]
+#[candid_method(query)]
+#[export_name = "transform"]
+async fn transform(raw: CanisterHttpResponsePayload) -> CanisterHttpResponsePayload {
+    let mut sanitized = raw.clone();
+    RESPONSE_HEADERS_SANTIZATION.with(|response_headers_blacklist| {
+        let mut processed_headers = vec![];
+        for header in raw.headers.iter() {
+            if !response_headers_blacklist.contains(&header.name.as_str()) {
+                processed_headers.insert(0, header.clone());
+            }
+        }
+        sanitized.headers = processed_headers;
+    });
+    return sanitized;
+}
+
+fn main() {}
