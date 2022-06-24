b95ac535b2b4b8a63da75b60907a7b17f401641b
QJ <qijing.yu@dfinity.org>
2022-06-24 00:53:31 +0000

    200 mins of data per API call. 1 http_request every 5 heartbeats.

--- rust/exchange_rate/src/main.rs
+++ rust/exchange_rate/src/main.rs
@@ -1,8 +1,9 @@
-use candid::{candid_method, CandidType, Principal};
-use ic_cdk::{self};
-use ic_cdk_macros::{self, heartbeat, query, update};
+use candid::{CandidType, Principal};
+use ic_cdk::storage;
+use ic_cdk_macros::{self, heartbeat, post_upgrade, pre_upgrade, query, update};
 use serde::{Deserialize, Serialize};
-use std::cell::RefCell;
+use serde_json::{self, Value};
+use std::cell::{RefCell, RefMut};
 use std::collections::{HashMap, HashSet};
 
 type Timestamp = u64;
@@ -29,11 +30,14 @@ pub struct HttpHeader {
 #[derive(Clone, Debug, PartialEq, CandidType, Eq, Hash, Serialize, Deserialize)]
 pub enum HttpMethod {
     GET,
+    // POST,
+    // HEAD,
 }
 
 #[derive(CandidType, Deserialize, Debug)]
 pub struct CanisterHttpRequestArgs {
     pub url: String,
+    //pub max_response_bytes: Option<u64>,
     pub headers: Vec<HttpHeader>,
     pub body: Option<Vec<u8>>,
     pub http_method: HttpMethod,
@@ -51,11 +55,32 @@ pub struct CanisterHttpResponsePayload {
 // Given that 2MB is max-allow cansiter response size, and each <Timestamp, Rate> pair
 // should be less that 20 bytes. Maximum data points could be returned for each
 // call can be as many as 2MB / 20B = 1000000.
-pub const MAX_DATA_PONTS_COUNT: usize = 1000000;
+pub const MAX_DATA_PONTS_CANISTER_RESPONSE: usize = 1000000;
 
-// Remote fetch interval is always 60 secs. It is only the canister returned interval
+// Remote fetch interval in secs. It is only the canister returned interval
 // that is dynamic according to the data size needs to be returned.
-pub const REMOTE_FETCH_INTERVAL: i64 = 60;
+pub const REMOTE_FETCH_GRANULARITY: u64 = 60;
+
+// For how many rounds of heartbeat, make a http_request call.
+pub const RATE_LIMIT_FACTOR: usize = 5;
+
+// How many data points in each Coinbase API call. Maximum allowed is 300
+pub const DATA_POINTS_PER_API: u64 = 200;
+
+// Maximum raw Coinbase API response size. This field is used by IC to calculate circles cost per HTTP call.
+// Here is how this number is derived:
+// Each Coinbase API call return an array of array, and each sub-array look like below:
+// [
+//     1652454000,
+//     9.51,
+//     9.6,
+//     9.55,
+//     9.54,
+//     4857.1892
+// ],
+// Each field of this sub-arry takes less than 10 bytes. Then,
+// 10 (bytes per field) * 6 (fields per timestamp) * 200 (timestamps)
+pub const MAX_RESPONSE_BYTES: u64 = 10 * 6 * DATA_POINTS_PER_API;
 
 pub const RESPONSE_HEADERS_SANTIZATION: [&'static str; 7] = [
     "Date",                    // DateTime of the request is made
@@ -70,14 +95,24 @@ pub const RESPONSE_HEADERS_SANTIZATION: [&'static str; 7] = [
 thread_local! {
     pub static FETCHED: RefCell<HashMap<Timestamp, Rate>>  = RefCell::new(HashMap::new());
     pub static REQUESTED: RefCell<HashSet<Timestamp>> = RefCell::new(HashSet::new());
-    pub static HEARTBEAT_COUNT: RefCell<i32> = RefCell::new(0);
+    pub static RATE_COUNTER: RefCell<usize> = RefCell::new(0);
 }
 
 // Canister heartbeat. Process one item in queue
 // #[export_name = "canister_heartbeat"]
 #[heartbeat]
 async fn heartbeat() {
-    get_next_rate().await;
+    let mut should_fetch = false;
+    RATE_COUNTER.with(|counter| {
+        let state = counter.clone().into_inner();
+        if state == 0 {
+            should_fetch = true;
+        }
+        counter.replace((state + 1) % RATE_LIMIT_FACTOR);
+    });
+    if should_fetch {
+        get_next_rate().await;
+    }
 }
 
 /*
@@ -85,13 +120,12 @@ Get rates for a time range defined by start time and end time. This function can
 as HTTP update call.
 */
 #[update]
-#[candid_method(update)]
 async fn get_rates(range: TimeRange) -> RatesWithInterval {
     // round down start time and end time to the minute (chop off seconds), to be checked in the hashmap
 
     // normalize the start_time and end_time to the minute before query remote.
-    let start_min = range.start / 60;
-    let end_min = range.end / 60;
+    let start_min = range.start / REMOTE_FETCH_GRANULARITY;
+    let end_min = range.end / REMOTE_FETCH_GRANULARITY;
 
     // compose a return structure
     let mut fetched = HashMap::new();
@@ -99,11 +133,8 @@ async fn get_rates(range: TimeRange) -> RatesWithInterval {
     // pull available ranges from hashmap
     FETCHED.with(|map| {
         let map = map.borrow();
-        ic_cdk::api::print("In Fetched.");
         for requested_min in start_min..end_min {
-            ic_cdk::api::print("In iterations");
-
-            let requested = requested_min * 60;
+            let requested = requested_min * REMOTE_FETCH_GRANULARITY;
             if map.contains_key(&requested) {
                 // The fetched slot is within user requested range. Add to result for later returning.
                 ic_cdk::api::print(format!("Found {} in map!", requested));
@@ -112,11 +143,7 @@ async fn get_rates(range: TimeRange) -> RatesWithInterval {
                 ic_cdk::api::print(format!("Did not find {} in map!", requested));
                 // asynchoronously request downloads for unavailable ranges
 
-                // Simply putting the request to request queue. This queue will likely
-                // have duplicate entries, if users request same range of data multiple times.
-                // Double downloading is avoided right before the time of downloading by checking
-                // whether the data already exists in FETCHED map.
-                add_job_to_queue(requested);
+                add_job_to_job_set(requested);
             }
         }
     });
@@ -137,9 +164,9 @@ fn sample_with_interval(fetched: HashMap<Timestamp, Rate>) -> RatesWithInterval 
         60 * 24, // 1 data point every day
     ];
     for i in interval_options {
-        if fetched.len() / i < MAX_DATA_PONTS_COUNT {
+        if fetched.len() / i < MAX_DATA_PONTS_CANISTER_RESPONSE {
             return RatesWithInterval {
-                interval: i * 60,
+                interval: i * REMOTE_FETCH_GRANULARITY as usize,
                 rates: fetched
                     .into_iter()
                     .filter(|r| r.0 % (i as u64) == 0)
@@ -147,24 +174,27 @@ fn sample_with_interval(fetched: HashMap<Timestamp, Rate>) -> RatesWithInterval 
             };
         }
     }
-    panic!("This shouldn't be happening! Couldn't find a inteval that can keep total data points count in {}.", MAX_DATA_PONTS_COUNT);
+    panic!("This shouldn't be happening! Couldn't find a inteval that can keep total data points count in {}.", MAX_DATA_PONTS_CANISTER_RESPONSE);
 }
 
-fn add_job_to_queue(job: Timestamp) -> () {
+fn add_job_to_job_set(job: Timestamp) -> () {
+    // Since Coinbase API allows DATA_POINTS_PER_API data points (5 hours of data) per API call,
+    // and the response size is roughly 14KB, which is way below max_response_size,
+    // we normalize the job to the beginning of 5 hours.
     REQUESTED.with(|set| {
         let mut set = set.borrow_mut();
-        set.insert(job);
+        let normalized_job = job / (REMOTE_FETCH_GRANULARITY * DATA_POINTS_PER_API)
+            * (REMOTE_FETCH_GRANULARITY * DATA_POINTS_PER_API);
+        set.insert(normalized_job);
     });
 }
 
 /*
-Register the cron job which takes the tip of the queue, and sends a canister call to self.
+Register the cron job which takes the tip of the job set, and sends a canister call to self.
 Potentially, different nodes executing the canister will trigger different job during the same period.
 The idea is to gap the cron job with large enough time gap, so they won't trigger remote service side
 rate limiting.
  */
-// #[update]
-// #[candid_method(update)]
 async fn get_next_rate() {
     let mut job_id: u64 = 0;
 
@@ -209,12 +239,12 @@ async fn get_next_rate() {
 }
 
 /*
-A function to call IC http_request function with a single minute range.
-This function is to be triggered by timer as jobs move to the tip of the queue.
+A function to call IC http_request function with sample interval of REMOTE_FETCH_GRANULARITY seconds. Each API
+call fetches DATA_POINTS_PER_API data points, which is equivalent of DATA_POINTS_PER_API minutes of data.
  */
 async fn get_rate(job: Timestamp) {
-    let start_timestamp = job as i64;
-    let end_timestamp = (job + 59) as i64;
+    let start_timestamp = job;
+    let end_timestamp = job + REMOTE_FETCH_GRANULARITY * DATA_POINTS_PER_API;
 
     let host = "api.pro.coinbase.com";
     let mut host_header = host.clone().to_owned();
@@ -230,13 +260,14 @@ async fn get_rate(job: Timestamp) {
             value: "exchange_rate_canister".to_string(),
         },
     ];
-    let url = format!("https://{host}/products/ICP-USD/candles?granularity=60&start={start_timestamp}&end={end_timestamp}");
+    let url = format!("https://{host}/products/ICP-USD/candles?granularity={REMOTE_FETCH_GRANULARITY}&start={start_timestamp}&end={end_timestamp}");
     ic_cdk::api::print(url.clone());
 
     let request = CanisterHttpRequestArgs {
         url: url,
         http_method: HttpMethod::GET,
         body: None,
+        //max_response_bytes: Some(MAX_RESPONSE_BYTES),
         transform_method_name: Some("transform".to_string()),
         headers: request_headers,
     };
@@ -261,11 +292,7 @@ async fn get_rate(job: Timestamp) {
                 let mut fetched = fetched.borrow_mut();
                 let decoded_body = String::from_utf8(decoded_result.body)
                     .expect("Remote service response is not UTF-8 encoded.");
-                ic_cdk::api::print(format!("Got decoded result: {}", decoded_body));
-                let close_rate = decoded_body.split(",").into_iter().collect::<Vec<&str>>()[4]
-                    .parse::<Rate>()
-                    .expect("Couldn't parse the rate to float.");
-                fetched.insert(job, close_rate);
+                decode_body_to_rates(&decoded_body, &mut fetched);
             });
         }
         Err((r, m)) => {
@@ -274,14 +301,22 @@ async fn get_rate(job: Timestamp) {
             ic_cdk::api::print(message.clone());
 
             // Since the remote request failed. Adding the de-queued job back again for retries.
-            add_job_to_queue(job);
+            add_job_to_job_set(job);
         }
     }
 }
 
+fn decode_body_to_rates(body: &str, fetched: &mut RefMut<HashMap<u64, f32>>) {
+    //ic_cdk::api::print(format!("Got decoded result: {}", body));
+    let rates_array: Vec<Vec<Value>> = serde_json::from_str(&body).unwrap();
+    for rate in rates_array {
+        let timestamp = rate[0].as_u64().expect("Couldn't parse the timestamp.");
+        let close_rate = rate[4].as_f64().expect("Couldn't parse the rate.");
+        fetched.insert(timestamp as Timestamp, close_rate as Rate);
+    }
+}
+
 #[query]
-#[candid_method(query)]
-#[export_name = "transform"]
 async fn transform(raw: CanisterHttpResponsePayload) -> CanisterHttpResponsePayload {
     let mut sanitized = raw.clone();
     let mut processed_headers = vec![];
@@ -294,4 +329,57 @@ async fn transform(raw: CanisterHttpResponsePayload) -> CanisterHttpResponsePayl
     return sanitized;
 }
 
+#[pre_upgrade]
+fn pre_upgrade() {
+    FETCHED.with(|fetched| storage::stable_save((fetched,)).unwrap());
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+    let (old_fetched,): (HashMap<Timestamp, Rate>,) = storage::stable_restore().unwrap();
+    FETCHED.with(|fetched| *fetched.borrow_mut() = old_fetched);
+}
+
 fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_decode_body_to_rates() {
+        let body = "
+[
+    [
+        1652454300,
+        9.51,
+        9.59,
+        9.54,
+        9.51,
+        14184.2377
+    ],
+    [
+        1652454240,
+        9.51,
+        9.55,
+        9.55,
+        9.52,
+        2385.9735
+    ],
+    [
+        1652454180,
+        9.54,
+        9.58,
+        9.55,
+        9.56,
+        1930.129
+    ]
+]
+        ";
+        let results = RefCell::new(HashMap::<Timestamp, Rate>::new());
+        let mut fetched = results.borrow_mut();
+        decode_body_to_rates(body, &mut fetched);
+        assert!(fetched.len() == 3);
+        assert!(fetched.get(&1652454180) == Some(&(9.56 as f32)));
+    }
+}
