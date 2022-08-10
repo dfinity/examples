70c702aac508003f5f8fdb41328b75f450e7cb41
Severin Siffert <severin.siffert@dfinity.org>
2022-08-10 09:34:08 +0200

    remove useless import

--- rust/hello/webpack.config.js
+++ rust/hello/webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 
 function initCanisterEnv() {
   let localCanisters, prodCanisters;
