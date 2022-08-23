ce5f7b192a09df272ae2744e4ae31af61c2136d8
Thomas Locher <thomas.locher@dfinity.org>
2022-08-23 16:02:17 +0200

    Removed the obsolete upgrade_management.rs file.

--- rust/bitcoin_wallet/src/bitcoin_wallet/upgrade_management.rs
+++
@@ -1,8 +0,0 @@
-use crate::types::{
-    from_bitcoin_network_to_types_network, from_types_network_to_bitcoin_network,
-    AddressUsingPrimitives,
-};
-use bitcoin::{Address, Network};
-use std::str::FromStr;
-
-
