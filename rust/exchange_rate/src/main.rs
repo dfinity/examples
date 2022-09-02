53dbf49d6c7032b81e6e604410f10e960e9951c9
QJ <qijing.yu@dfinity.org>
2022-09-02 21:29:26 +0000

    add security response headers to backend canister.

--- rust/exchange_rate/src/main.rs
+++ rust/exchange_rate/src/main.rs
@@ -315,7 +315,32 @@ fn decode_body_to_rates(body: &str, fetched: &mut RefMut<HashMap<u64, f32>>) {
 #[query]
 async fn transform(raw: CanisterHttpResponsePayload) -> CanisterHttpResponsePayload {
     let mut sanitized = raw.clone();
-    sanitized.headers = vec![];
+    sanitized.headers = vec![
+        HttpHeader {
+            name: "Content-Security-Policy".to_string(),
+            value: "default-src 'self'".to_string(),
+        },
+        HttpHeader {
+            name: "Referrer-Policy".to_string(),
+            value: "strict-origin".to_string(),
+        },
+        HttpHeader {
+            name: "Permissions-Policy".to_string(),
+            value: "geolocation=(self)".to_string(),
+        },
+        HttpHeader {
+            name: "Strict-Transport-Security".to_string(),
+            value: "max-age=63072000".to_string(),
+        },
+        HttpHeader {
+            name: "X-Frame-Options".to_string(),
+            value: "DENY".to_string(),
+        },
+        HttpHeader {
+            name: "X-Content-Type-Options".to_string(),
+            value: "nosniff".to_string(),
+        },
+    ];
     sanitized
 }
 
