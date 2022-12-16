eb7a792ba48ea38b8460cb7fd7f48c863ea58bb8
Kepler Vital <kepler.vital@dfinity.org>
2022-12-16 10:57:31 +0100

    update Makefile to include tests

--- motoko/ios-notifications/dapp-demo/test_api.sh
+++ motoko/ios-notifications/dapp-demo/test_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(dfx canister call ios_notifications_api whoami | grep -e "$(dfx identity get-principal)") ]; then 
+if echo $(dfx canister call ios_notifications_api whoami) | grep -q -e "$(dfx identity get-principal)"; then 
     echo "Test passed!"; 
 else
     echo "Test failed :-("
