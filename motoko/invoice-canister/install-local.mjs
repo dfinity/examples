3b6da0bb3ef3a3924c04b88ca258b3862a65fbd9
Ashton Engberg <atengberg@gmail.com>
2022-12-27 07:05:06 -0500

    part 1 of finishing changes for fixing pr--new script, added pem, deleted unnecessary files

--- motoko/invoice-canister/install-local.mjs
+++ motoko/invoice-canister/install-local.mjs
@@ -1,52 +1,52 @@
 import { $ } from "zx";
 
-// await $.spawn(
-//   `set -e
+// add test to verify networks.json is configured to use nns ledger? 
+// this script will error out if not networks.json is not configured
+// as dfx nns install will fail
 
-//     dfx stop
-//     dfx start --background --clean`
-// );
+// zx spawn doesn't handle detached, unref of spawned child processes 
+// -> https://github.com/google/zx/discussions/371. could unwrap
+// but simpler to use lower level blocking api atm
+import { spawnSync } from "child_process"
 
-const hasMinter = (await $`dfx identity list`).stdout.includes("minter");
-console.log(hasMinter);
-let MINT_ACC;
+await $`dfx stop`
 
-// Setup minter and set MINT_ACC
-if (!hasMinter) {
-  await $`dfx identity new minter --disable-encryption
-    dfx identity use minter`;
-  MINT_ACC = await $`dfx ledger account-id`;
-} else {
-  await $`dfx identity use minter`;
-  MINT_ACC = await $`dfx ledger account-id`;
-}
+spawnSync(
+  'dfx',
+  ['start', '--clean', '--background'],
+  { 
+    detached: true,
+    stdio: 'ignore'
+  }
+)
 
-// Switch back to default
-await $`dfx identity use default`;
+await $`dfx nns install`
 
-const LEDGER_ACC = (await $`dfx ledger account-id`).stdout;
-const TEST_ACC =
-  "cd60093cef12e11d7b8e791448023348103855f682041e93f7d0be451f48118b";
+// ident-1.pem is one of two identities nns ledger initiallized by sending ICP funds to, going to remove after transfering
+// funds to E2E test account identity, balanceHolder. E2E tests are atm simplified so that ICP is transfered back and forth
+// between subaccounts controlled by the invoice canister, as opposed to explicitly invoking the ICP ledger canister
+await $`dfx identity import invoice-nns-initiallized-icp-funded-identity test/e2e/src/utils/ident-1.pem --disable-encryption`
+await $`dfx identity use invoice-nns-initiallized-icp-funded-identity`
+// note this account identifier corresponds to the balance holder's principal-subaccount of the invoice canister
+await $`dfx ledger transfer 675d3f2043c6bf5d642454cf0890b467673f0bacfd1c85882c0d650d4c6d2abb --icp 1000 --memo 0`
 
-await $`
-# Use private api for install
-rm src/ledger/ledger.did
-cp src/ledger/ledger.private.did src/ledger/ledger.did`;
+await $`dfx identity use default`
+// to keep things clean, dev can always import later if needed
+await $`dfx identity remove invoice-nns-initiallized-icp-funded-identity`
 
 try {
-
-
-  await $`dfx deploy ledger --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } }; record { "'${TEST_ACC}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})'`;
-
-  // dfx deploy ledger --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } }; record { "'${TEST_ACC}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})'
-
-  // # Replace with public api
-  await $`
-rm src/ledger/ledger.did
-cp src/ledger/ledger.public.did src/ledger/ledger.did
-
-dfx deploy invoice
-`;
-} catch (error) {
-  console.error(error);
+  await $`dfx deploy invoice`
+} catch (expected) {
+  // this error is related to that the nns-ledger canister id is not available to dfx when building the invoice
+  // the first time the invoice canister is created and built after dfx start --clean; as there's no defined canister id 
+  // in /.dfx/canister_ids.json for the ledger canister, the other option is to not include it as a dependency in dfx.json 
+  // and import in ICPLedger.mo and instead add a Motoko typing reference to instantiate it directly in Motoko as an actor 
+  // by its local canister id as provided by the dfx nns commands
+  console.log("error related to importing nns-ledger is expected, atm not sure the reason this happens")
 }
+
+// now deploying invoice will succeed
+await $`dfx deploy invoice`
+
+// and finally generate declarations for E2E testing
+await $`dfx generate invoice`
