74e0deaf4efd1af9be9f64922e18aba93e2b53b9
Ashton Engberg <atengberg@gmail.com>
2023-01-07 15:24:46 -0500

    updated install-local script to reflect new deployment argument, cleaned up output

--- motoko/invoice-canister/install-local.mjs
+++ motoko/invoice-canister/install-local.mjs
@@ -1,26 +1,71 @@
-import { $ } from "zx";
+import { $, argv, chalk } from "zx";
+import { spawnSync } from "child_process"
 
 // add test to verify networks.json is configured to use nns ledger? 
 // this script will error out if not networks.json is not configured
 // as dfx nns install will fail
 
-// zx spawn doesn't handle detached, unref of spawned child processes 
-// -> https://github.com/google/zx/discussions/371. could unwrap
-// but simpler to use lower level blocking api atm
-import { spawnSync } from "child_process"
+/*
+* Script to spin up fresh replica with:
+*   -nns-ledger installed by dfx nns command
+*   -invoice canister deployed with or without passed delegatedAdministrator 
+*     >> to specify a delegatedAministrator use the flag --deployWDelegation 
+*           by default is set as the Ed25519KeyIdentity the nns-ledger initializes with funds
+*           or *TODO* --deployWDelegation=<principal> to pass a principal of your own choosing.
+*
+* Call examples:
+* 
+*  >> node ./install-local.mjs 
+*  >> node ./install-local.mjs --deployWDelegation
+*
+*  Note:
+*  Currently in this script the other Secp256k1KeyIdentity the nns-ledger is initialized sending
+*   funds to is added to dfx identity list to transfer funds to the balance holder identity used
+*   in the E2E tests, and then removed to keep things clean; note this logic will be moved to a 
+*   pretest specific script in the future.
+*   (*TODO* add a flag to optionally install this Secp256k1KeyIdentity identity for the user)
+*/
 
+console.info(chalk.bgBlue("restarting clean to reset nns-ledger"))
 await $`dfx stop`
-
 spawnSync(
-  'dfx',
-  ['start', '--clean', '--background'],
-  { 
-    detached: true,
-    stdio: 'ignore'
-  }
+    'dfx',
+    ['start', '--clean', '--background'],
+    { 
+        detached: true,
+        stdio: 'ignore'
+    }
 )
-
 await $`dfx nns install`
+try {
+    await $`dfx canister create invoice`
+    await $`dfx build invoice`
+  } catch (error) {
+    const { _stderr } = error;
+    if (_stderr.includes(`examples/motoko/invoice-canister/.dfx/local/canisters/idl/ryjl3-tyaaa-aaaaa-aaaba-cai.did. This may produce errors during the build.`)
+        && _stderr.includes(`examples/motoko/invoice-canister/src/invoice/ICPLedger.mo:1.1-1.36: import error [M0009]`) 
+        && _stderr.includes(`WARN: Failed to copy canister candid from`)
+        && _stderr.includes(`examples/motoko/invoice-canister/.dfx/local/canisters/idl/ryjl3-tyaaa-aaaaa-aaaba-cai.did" does not exist`)
+    ) {
+        console.warn(chalk.yellow(`Error due to import of nns-ledger is expected first time invoice canister is freshly built: should deploy successfully this time...`));
+    } else {
+        // was something else abort! 
+        throw new Error(error)
+    }
+}
+
+if (argv.deployWDelegation) {
+// the principal "jg6qm-uw64t-m6ppo-oluwn-ogr5j-dc5pm-lgy2p-eh6px-hebcd-5v73i-nqe" is that of the Ed25519KeyIdentity 
+// the nns-ledger is initiallized sending ~10000000 ICP to it; is also used as the delegated adminstrator in E2E tests who 
+// besides the original deployer of the canister has the power to add and remove principals from the allowed creators list 
+    console.info(chalk.bgBlue("\ndeploying invoice canister with delegated administrator\n"))
+    await $`dfx deploy invoice --argument '( opt principal"jg6qm-uw64t-m6ppo-oluwn-ogr5j-dc5pm-lgy2p-eh6px-hebcd-5v73i-nqe" )'`
+} else {
+    await $`dfx deploy invoice`
+}
+
+// todo // todo // todo // todo // todo // todo // todo // todo // todo // todo // todo // todo // todo move these into pretest
+// can use Ed25519KeyIdentity js above when move into tests, will still include the ident-1.pem file for development
 
 // ident-1.pem is one of two identities nns ledger initiallized by sending ICP funds to, going to remove after transfering
 // funds to E2E test account identity, balanceHolder. E2E tests are atm streamlined so that ICP is transfered back and forth
@@ -29,24 +74,10 @@ await $`dfx identity import invoice-nns-initiallized-icp-funded-identity test/e2
 await $`dfx identity use invoice-nns-initiallized-icp-funded-identity`
 // note this account identifier corresponds to the balance holder's principal-subaccount of the invoice canister
 await $`dfx ledger transfer 675d3f2043c6bf5d642454cf0890b467673f0bacfd1c85882c0d650d4c6d2abb --icp 1000 --memo 0`
-
 await $`dfx identity use default`
 // to keep things clean, dev can always import later if needed
 await $`dfx identity remove invoice-nns-initiallized-icp-funded-identity`
 
-try {
-  await $`dfx deploy invoice`
-} catch (expected) {
-  // this error is related to that the nns-ledger canister id is not available to dfx when building the invoice
-  // the first time the invoice canister is created and built after dfx start --clean; as there's no defined canister id 
-  // in /.dfx/canister_ids.json for the ledger canister, the other option is to not include it as a dependency in dfx.json 
-  // and import in ICPLedger.mo and instead add a Motoko typing reference to instantiate it directly in Motoko as an actor 
-  // by its local canister id as provided by the dfx nns commands
-  console.log("error related to importing nns-ledger is expected, atm not sure the reason this happens")
-}
-
-// now deploying invoice will succeed
-await $`dfx deploy invoice`
-
-// and finally generate declarations for E2E testing
 await $`dfx generate invoice`
+
+console.info(chalk.bgBlue("\nInvoice canister ready...\n"))
