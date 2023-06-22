21c103aa2ae7367af1db95d1c32ebe8424bc1cc1
Islam El-Ashi <islam.elashi@dfinity.org>
2023-06-22 13:37:28 +0300

    update to 0.9.2

--- rust/basic_bitcoin/src/basic_bitcoin/src/ecdsa_api.rs
+++ rust/basic_bitcoin/src/basic_bitcoin/src/ecdsa_api.rs
@@ -3,8 +3,6 @@ use ic_cdk::api::management_canister::ecdsa::{
     SignWithEcdsaArgument, SignWithEcdsaResponse,
 };
 
-const SIGN_WITH_ECDSA_FEE: u128 = 26153846153;
-
 /// Returns the ECDSA public key of this canister at the given derivation path.
 pub async fn ecdsa_public_key(key_name: String, derivation_path: Vec<Vec<u8>>) -> Vec<u8> {
     // Retrieve the public key of this canister at the given derivation path
@@ -28,17 +26,14 @@ pub async fn sign_with_ecdsa(
     derivation_path: Vec<Vec<u8>>,
     message_hash: Vec<u8>,
 ) -> Vec<u8> {
-    let res: (SignWithEcdsaResponse,) = ecdsa::sign_with_ecdsa(
-        SignWithEcdsaArgument {
-            message_hash,
-            derivation_path,
-            key_id: EcdsaKeyId {
-                curve: EcdsaCurve::Secp256k1,
-                name: key_name,
-            },
+    let res: (SignWithEcdsaResponse,) = ecdsa::sign_with_ecdsa(SignWithEcdsaArgument {
+        message_hash,
+        derivation_path,
+        key_id: EcdsaKeyId {
+            curve: EcdsaCurve::Secp256k1,
+            name: key_name,
         },
-        SIGN_WITH_ECDSA_FEE,
-    )
+    })
     .await
     .unwrap();
 
