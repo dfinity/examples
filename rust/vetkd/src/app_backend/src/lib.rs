8065c3eca643c66930a503da6c21fc471ecea07e
Franz-Stefan Preiss <franzstefan@dfinity.org>
2023-08-09 16:56:04 +0200

    Extend vetKD example dapps with identity-based encryption (IBE) (#603)
    Extend the vetKD example dapps (both for Rust and Motoko) to demo identity-based encryption (IBE).

--- rust/vetkd/src/app_backend/src/lib.rs
+++ rust/vetkd/src/app_backend/src/lib.rs
@@ -48,6 +48,45 @@ async fn encrypted_symmetric_key_for_caller(encryption_public_key: Vec<u8>) -> S
     hex::encode(response.encrypted_key)
 }
 
+#[update]
+async fn ibe_encryption_key() -> String {
+    let request = VetKDPublicKeyRequest {
+        canister_id: None,
+        derivation_path: vec![b"ibe_encryption".to_vec()],
+        key_id: bls12_381_test_key_1(),
+    };
+
+    let (response,): (VetKDPublicKeyReply,) = ic_cdk::api::call::call(
+        vetkd_system_api_canister_id(),
+        "vetkd_public_key",
+        (request,),
+    )
+    .await
+    .expect("call to vetkd_public_key failed");
+
+    hex::encode(response.public_key)
+}
+
+#[update]
+async fn encrypted_ibe_decryption_key_for_caller(encryption_public_key: Vec<u8>) -> String {
+    let request = VetKDEncryptedKeyRequest {
+        derivation_id: ic_cdk::caller().as_slice().to_vec(),
+        public_key_derivation_path: vec![b"ibe_encryption".to_vec()],
+        key_id: bls12_381_test_key_1(),
+        encryption_public_key,
+    };
+
+    let (response,): (VetKDEncryptedKeyReply,) = ic_cdk::api::call::call(
+        vetkd_system_api_canister_id(),
+        "vetkd_encrypted_key",
+        (request,),
+    )
+    .await
+    .expect("call to vetkd_encrypted_key failed");
+
+    hex::encode(response.encrypted_key)
+}
+
 fn bls12_381_test_key_1() -> VetKDKeyId {
     VetKDKeyId {
         curve: VetKDCurve::Bls12_381,
