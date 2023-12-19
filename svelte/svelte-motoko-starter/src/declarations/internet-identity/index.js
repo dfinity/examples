e4fc680aa4a3365b4ccb663d3d5a45d4d7fb7bfc
Kyle Peacock <kylpeacock@gmail.com>
2023-12-19 14:22:48 -0800

    fix: updates svelte-starter-motoko example

--- svelte/svelte-motoko-starter/src/declarations/internet-identity/index.js
+++ svelte/svelte-motoko-starter/src/declarations/internet-identity/index.js
@@ -1,17 +1,17 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
 
 // Imports and re-exports candid interface
-import { idlFactory } from './backend.did.js';
-export { idlFactory } from './backend.did.js';
+import { idlFactory } from './internet-identity.did.js';
+export { idlFactory } from './internet-identity.did.js';
 // CANISTER_ID is replaced by webpack based on node environment
-export const canisterId = process.env.BACKEND_CANISTER_ID;
+export const canisterId = process.env.INTERNET-IDENTITY_CANISTER_ID;
 
 /**
  * @deprecated since dfx 0.11.1
  * Do not import from `.dfx`, instead switch to using `dfx generate` to generate your JS interface.
  * @param {string | import("@dfinity/principal").Principal} canisterId Canister ID of Agent
  * @param {{agentOptions?: import("@dfinity/agent").HttpAgentOptions; actorOptions?: import("@dfinity/agent").ActorConfig} | { agent?: import("@dfinity/agent").Agent; actorOptions?: import("@dfinity/agent").ActorConfig }} [options]
- * @return {import("@dfinity/agent").ActorSubclass<import("./backend.did.js")._SERVICE>}
+ * @return {import("@dfinity/agent").ActorSubclass<import("./internet-identity.did.js")._SERVICE>}
  */
 export const createActor = (canisterId, options = {}) => {
   console.warn(`Deprecation warning: you are currently importing code from .dfx. Going forward, refactor to use the dfx generate command for JavaScript bindings.
@@ -36,7 +36,7 @@ See https://internetcomputer.org/docs/current/developer-docs/updates/release-not
 };
   
 /**
- * A ready-to-use agent for the backend canister
- * @type {import("@dfinity/agent").ActorSubclass<import("./backend.did.js")._SERVICE>}
+ * A ready-to-use agent for the internet-identity canister
+ * @type {import("@dfinity/agent").ActorSubclass<import("./internet-identity.did.js")._SERVICE>}
  */
-export const backend = createActor(canisterId);
+export const internet-identity = createActor(canisterId);
