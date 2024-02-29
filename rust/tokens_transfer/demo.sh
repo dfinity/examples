619ed90c30c795a25a7710899d0b7b50e737d4b5
Moritz Fuller <moritz.fuller@dfinity.org>
2024-02-29 13:58:36 +0100

    🩹 fixed canister name

--- rust/tokens_transfer/demo.sh
+++ rust/tokens_transfer/demo.sh
@@ -36,7 +36,7 @@ dfx deploy tokens_transfer_backend
 
 TOKENS_TRANSFER_ACCOUNT_ID="$(dfx ledger account-id --of-canister tokens_transfer_backend)"
 TOKENS_TRANSFER_ACCOUNT_ID_BYTES="$(python3 -c 'print("vec{" + ";".join([str(b) for b in bytes.fromhex("'$TOKENS_TRANSFER_ACCOUNT_ID'")]) + "}")')"
-dfx canister call ledger transfer "(record { to=${TOKENS_TRANSFER_ACCOUNT_ID_BYTES}; amount=record { e8s=100_000 }; fee=record { e8s=10_000 }; memo=0:nat64; }, )"
+dfx canister call icp_ledger_canister transfer "(record { to=${TOKENS_TRANSFER_ACCOUNT_ID_BYTES}; amount=record { e8s=100_000 }; fee=record { e8s=10_000 }; memo=0:nat64; }, )"
 
 dfx canister call tokens_transfer_backend transfer "(record { amount=record { e8s=5 }; to_principal=principal \"$(dfx identity get-principal)\" },)"
 
