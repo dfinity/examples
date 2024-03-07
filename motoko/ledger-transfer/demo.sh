717b2b47cda62b3c1ef09f800fd9d93db9d782c0
Mathias Björkqvist <mathias.bjorkqvist@dfinity.org>
2024-03-07 13:57:13 +0100

    Update path to ledger.did and unify private and public

--- motoko/ledger-transfer/demo.sh
+++ motoko/ledger-transfer/demo.sh
@@ -8,17 +8,15 @@ trap 'dfx stop' EXIT
 export IC_VERSION=98eb213581b239c3829eee7076bea74acad9937b
 test -f ledger.wasm.gz || curl -o ledger.wasm.gz https://download.dfinity.systems/ic/${IC_VERSION}/canisters/ledger-canister_notify-method.wasm.gz
 test -f ledger.wasm || gunzip ledger.wasm.gz
-test -f ledger.private.did || curl -o ledger.private.did https://raw.githubusercontent.com/dfinity/ic/${IC_VERSION}/rs/rosetta-api/ledger.did
-test -f ledger.public.did || curl -o ledger.public.did https://raw.githubusercontent.com/dfinity/ic/${IC_VERSION}/rs/rosetta-api/ledger_canister/ledger.did
+test -f ledger.did || curl -o ledger.did https://raw.githubusercontent.com/dfinity/ic/${IC_VERSION}/rs/rosetta-api/icp_ledger/ledger.did
 
 dfx start --background --clean
 dfx identity new alice --disable-encryption || true
-cat <<<"$(jq '.canisters.ledger.candid="ledger.private.did"' dfx.json)" >dfx.json
+cat <<<"$(jq '.canisters.ledger.candid="ledger.did"' dfx.json)" >dfx.json
 export MINT_ACC=$(dfx --identity anonymous ledger account-id)
 export LEDGER_ACC=$(dfx ledger account-id)
 export ARCHIVE_CONTROLLER=$(dfx identity get-principal)
 dfx deploy ledger --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})'
-cat <<<"$(jq '.canisters.ledger.candid="ledger.public.did"' dfx.json)" >dfx.json
 dfx canister call ledger account_balance '(record { account = '$(python3 -c 'print("vec{" + ";".join([str(b) for b in bytes.fromhex("'$LEDGER_ACC'")]) + "}")')' })'
 
 dfx deploy ledger_transfer
