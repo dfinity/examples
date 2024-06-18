248600567a0706edf97ac01dc92508714230a338
Ulan Degenbaev <ulan.degenbaev@dfinity.org>
2024-06-18 12:14:23 +0000

    Adjustments

--- rust/qrcode/src/qrcode_backend/src/lib.rs
+++ rust/qrcode/src/qrcode_backend/src/lib.rs
@@ -5,7 +5,7 @@ use std::include_bytes;
 mod core;
 
 const IMAGE_SIZE_IN_PIXELS: usize = 1024;
-const QR_CODE_NUMBER_OF_ITERATIONS: usize = 20;
+const QR_CODE_NUMBER_OF_ITERATIONS: usize = 10;
 const LOGO_TRANSPARENT: &[u8] = include_bytes!("../assets/logo_transparent.png");
 const LOGO_WHITE: &[u8] = include_bytes!("../assets/logo_white.png");
 
@@ -20,12 +20,12 @@ struct Options {
     add_transparency: Option<bool>,
 }
 
-#[derive(CandidType, Deserialize)]
+#[derive(Clone, CandidType, Deserialize)]
 struct QrError {
     message: String,
 }
 
-#[derive(CandidType, Deserialize)]
+#[derive(Clone, CandidType, Deserialize)]
 enum QrResult {
     Image(Vec<u8>),
     Err(QrError),
@@ -51,21 +51,28 @@ fn qrcode_impl(input: String, options: Options) -> QrResult {
 }
 
 #[ic_cdk::update]
-fn qrcode(input: String, options: Options) {
+fn qrcode(input: String, options: Options) -> QrResult {
     let mut res = vec![];
 
     for _ in 0..QR_CODE_NUMBER_OF_ITERATIONS {
         res.push(qrcode_impl(input.clone(), options.clone()));
     }
 
+    let r = res[0].clone();
+
     STATE.with(|state| {
         state.borrow_mut().extend(res);
     });
+    r
 }
 
 #[ic_cdk::query]
 fn qrcode_query(input: String, options: Options) -> QrResult {
-    qrcode_impl(input, options)
+    let mut r = None;
+    for _ in 0..QR_CODE_NUMBER_OF_ITERATIONS {
+        r = Some(qrcode_impl(input.clone(), options.clone()));
+    }
+    r.unwrap()
 }
 
 #[ic_cdk::query]
