88e574173c94ed5a2dc610ff1f4f17c3ea1e5bd1
sa-github-api <138766536+sa-github-api@users.noreply.github.com>
2024-07-09 15:54:16 +0200

    Update commit of IC artifacts (#933)

--- .github/workflows/update-dfx-cache.sh
+++ .github/workflows/update-dfx-cache.sh
@@ -3,6 +3,7 @@
 # Download latest ic artifacts
 curl -O "https://download.dfinity.systems/ic/$current_sha/binaries/$arch/replica.gz"
 curl -O "https://download.dfinity.systems/ic/$current_sha/binaries/$arch/canister_sandbox.gz"
+curl -O "https://download.dfinity.systems/ic/$current_sha/binaries/$arch/compiler_sandbox.gz"
 curl -O "https://download.dfinity.systems/ic/$current_sha/binaries/$arch/ic-admin.gz"
 curl -O "https://download.dfinity.systems/ic/$current_sha/binaries/$arch/ic-btc-adapter.gz"
 curl -O "https://download.dfinity.systems/ic/$current_sha/binaries/$arch/ic-https-outcalls-adapter.gz"
@@ -14,6 +15,7 @@ curl -O "https://download.dfinity.systems/ic/$current_sha/binaries/$arch/sns.gz"
 # Overwrite artifacts in dfx cache
 gzip -d replica.gz && chmod +x replica && mv replica $(dfx cache show)
 gzip -d canister_sandbox.gz && chmod +x canister_sandbox && mv canister_sandbox $(dfx cache show)
+gzip -d compiler_sandbox.gz && chmod +x compiler_sandbox && mv compiler_sandbox $(dfx cache show)
 gzip -d ic-starter.gz && chmod +x ic-starter && mv ic-starter $(dfx cache show)
 gzip -d sandbox_launcher.gz && chmod +x sandbox_launcher && mv sandbox_launcher $(dfx cache show)
 gzip -d ic-admin.gz && chmod +x ic-admin && mv ic-admin $(dfx cache show)
