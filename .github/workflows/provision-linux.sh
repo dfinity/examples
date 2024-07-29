00112d269daef0b6356e0256d0729e8710830c1e
Linwei Shang <linwei.shang@dfinity.org>
2024-07-29 10:43:00 -0400

    chore: update default dfx to 0.22.0

--- .github/workflows/provision-linux.sh
+++ .github/workflows/provision-linux.sh
@@ -13,7 +13,7 @@ rm install-node.sh
 
 # Install DFINITY SDK.
 wget --output-document install-dfx.sh "https://raw.githubusercontent.com/dfinity/sdk/master/public/install-dfxvm.sh"
-DFX_VERSION=${DFX_VERSION:=0.21.0} DFXVM_INIT_YES=true bash install-dfx.sh
+DFX_VERSION=${DFX_VERSION:=0.22.0} DFXVM_INIT_YES=true bash install-dfx.sh
 rm install-dfx.sh
 echo "$HOME/.local/share/dfx/bin" >> $GITHUB_PATH
 source "$HOME/.local/share/dfx/env"
