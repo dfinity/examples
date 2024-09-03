e79533bcaf92a2b6659e09b8333e0ddab5a04ecd
Dragoljub Djuric <dragoljub.duric@dfinity.org>
2024-09-03 12:12:25 +0200

    Update build.sh

--- rust/basic_bitcoin/src/basic_bitcoin/build.sh
+++ rust/basic_bitcoin/src/basic_bitcoin/build.sh
@@ -18,18 +18,11 @@ else
 fi
 
 cargo install ic-wasm --version 0.2.0 --root ./
-STATUS=$?
 
-if [[ "$STATUS" -eq "0" ]]; then
-  ./bin/ic-wasm \
-      	"$SCRIPT_DIR/../../target/$TARGET/release/$CANISTER.wasm" \
-      	-o "$SCRIPT_DIR/../../target/$TARGET/release/$CANISTER.wasm" \
-      	metadata candid:service -f "$SCRIPT_DIR/basic_bitcoin.did" -v public
-  true
-else
-  echo Could not install ic-wasm
-  false
-fi
+./bin/ic-wasm \
+      "$SCRIPT_DIR/../../target/$TARGET/release/$CANISTER.wasm" \
+      -o "$SCRIPT_DIR/../../target/$TARGET/release/$CANISTER.wasm" \
+      metadata candid:service -f "$SCRIPT_DIR/basic_bitcoin.did" -v public
 
 popd
 
