babb086a9507e50cb829a9b642c776920fe43b0e
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2024-09-20 19:00:35 +0200

    move some code and work on README

--- rust/x509/src/x509_example_rust/src/wasm_only.rs
+++ rust/x509/src/x509_example_rust/src/wasm_only.rs
@@ -1,6 +1,6 @@
 use crate::SchnorrKeyName;
 
-use super::{PublicKeyReply, SchnorrAlgorithm};
+use super::SchnorrAlgorithm;
 use candid::{CandidType, Principal};
 use der::{asn1::BitString, pem::LineEnding, DecodePem, Encode, EncodePem};
 use ic_cdk::export_candid;
@@ -20,6 +20,122 @@ use x509_cert::{
 };
 
 type CanisterId = Principal;
+
+thread_local! {
+    static KEY_NAME: RefCell<SchnorrKeyName> = RefCell::new(SchnorrKeyName::DfxTestKey);
+
+    // cache the public key and certificate to avoid fetching them multiple times
+    static ROOT_CA_PUBLIC_KEY: OnceCell<Vec<u8>> = OnceCell::new();
+    static ROOT_CA_CERTIFICATE_PEM: OnceCell<String> = OnceCell::new();
+
+    static CHILD_CERTIFICATE_SERIAL_NUMBER: RefCell<u32> = RefCell::new(0);
+}
+
+#[init]
+fn init(key_name: SchnorrKeyName) {
+    KEY_NAME.with(|state| {
+        *state.borrow_mut() = key_name;
+    });
+}
+
+#[update]
+async fn root_ca_certificate() -> Result<X509CertificateString, String> {
+    // if the certificate is already cached, return it
+
+    if let Some(certificate) = ROOT_CA_CERTIFICATE_PEM.with(|inner| inner.get().map(|v| v.clone()))
+    {
+        return Ok(X509CertificateString {
+            x509_certificate_string: certificate,
+        });
+    }
+
+    // if the certificate is not cached, create it and try to cache it
+
+    let serial_number = SerialNumber::from(0u32);
+
+    let subject = Name::from_str(
+        "CN=Web3 certification authority corporation,O=Web3 ceritifcation authority Inc,C=US",
+    )
+    .unwrap();
+
+    let subject_public_key = der::asn1::BitString::new(0, root_ca_public_key_bytes().await?)
+        .map_err(|e| format!("source: {:?}", e.source()))?;
+
+    let pub_key = SubjectPublicKeyInfoOwned {
+        algorithm: public_key_algorithm_identifier(),
+        subject_public_key,
+    };
+
+    let newly_constructed_x509_certificate_string = pem_certificated_signed_by_root_ca(
+        Profile::Root,
+        serial_number,
+        validity(),
+        subject,
+        pub_key,
+    )
+    .await
+    .map_err(|e| format!("failed to create root certificate: {e:?}"))?;
+
+    let x509_certificate_string = ROOT_CA_CERTIFICATE_PEM.with(move |inner| {
+        inner
+            .get_or_init(|| newly_constructed_x509_certificate_string)
+            .clone()
+    });
+
+    Ok(X509CertificateString {
+        x509_certificate_string,
+    })
+}
+
+#[update]
+async fn child_certificate(
+    certificate_request_info: PemCertificateRequest,
+) -> Result<X509CertificateString, String> {
+    let cert_req =
+        CertReq::from_pem(certificate_request_info.pem_certificate_request.as_bytes())
+            .map_err(|e| format!("failed to parse PEM certificate signing request: {e:?}"))?;
+
+    verify_certificate_request_signature(&cert_req)?;
+
+    if !cert_req.info.attributes.is_empty() {
+        return Err("Attributes are currently not supported in this example".to_string());
+    }
+
+    prove_ownership(&cert_req, ic_cdk::api::caller() /*, ... */)?;
+
+    let root_certificate_pem = root_ca_certificate().await?;
+    let root_certificate =
+        x509_cert::Certificate::from_pem(root_certificate_pem.x509_certificate_string.as_str())
+            .map_err(|e| format!("failed to parse PEM root CA certificate: {e:?}"))?;
+
+    let profile = Profile::Leaf {
+        issuer: root_certificate.tbs_certificate.subject.clone(),
+        enable_key_agreement: false,
+        enable_key_encipherment: false,
+    };
+
+    let serial_number = SerialNumber::from(next_child_certificate_serial_number());
+
+    let x509_certificate_string = pem_certificated_signed_by_root_ca(
+        profile,
+        serial_number,
+        // For simplicity of this example, let's just use the same validity
+        // period as the root certificate. In a real application, the validity
+        // would normally not start in the past and might end well before the root
+        // certificate validity ends. Also, the validity of the child
+        // ceritifcate should always be in the time frame of the root
+        // certificate's validity.
+        root_certificate.tbs_certificate.validity.clone(),
+        cert_req.info.subject.clone(),
+        cert_req.info.public_key.clone(),
+    )
+    .await
+    .map_err(|e| format!("failed to create child certificate: {e:?}"))?;
+
+    Ok(X509CertificateString {
+        x509_certificate_string,
+    })
+}
 
 #[derive(CandidType, Serialize, Debug)]
 struct ManagementCanisterSchnorrPublicKeyRequest {
@@ -52,38 +168,14 @@ struct ManagementCanisterSignatureReply {
     pub signature: Vec<u8>,
 }
 
-thread_local! {
-    static KEY_NAME: RefCell<SchnorrKeyName> = RefCell::new(SchnorrKeyName::DfxTestKey);
-    static DERIVATION_PATH: RefCell<Vec<Vec<u8>>> = RefCell::new(vec![]);
-
-    static ROOT_CA_PUBLIC_KEY: OnceCell<Vec<u8>> = OnceCell::new();
-    static ROOT_CA_CERTIFICATE_PEM: OnceCell<String> = OnceCell::new();
-}
-
-#[init]
-fn init(key_name: SchnorrKeyName) {
-    KEY_NAME.with(|state| {
-        *state.borrow_mut() = key_name;
-    });
-}
-
-fn key_id() -> SchnorrKeyId {
-    KEY_NAME.with(|state| {
-        let name = String::from(<&'static str>::from(*state.borrow()));
-        SchnorrKeyId {
-            algorithm: SchnorrAlgorithm::Ed25519,
-            name,
-        }
-    })
-}
-
-fn derivation_path() -> Vec<Vec<u8>> {
-    DERIVATION_PATH.with(|state| state.borrow().clone())
-}
-
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct X509CertificateString {
     x509_certificate_string: String,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub struct PemCertificateRequest {
+    pem_certificate_request: String,
 }
 
 struct Signer {
@@ -145,55 +237,31 @@ impl DynSignatureAlgorithmIdentifier for Signer {
     }
 }
 
-#[update]
-async fn root_ca_certificate() -> Result<X509CertificateString, String> {
-    // if the certificate is already cached, return it
+async fn root_ca_public_key_bytes() -> Result<Vec<u8>, String> {
+    // if the public key is already cached, return it
+    if let Some(public_key) = ROOT_CA_PUBLIC_KEY.with(|inner| inner.get().map(|v| v.clone())) {
+        return Ok(public_key);
+    };
 
-    if let Some(certificate) = ROOT_CA_CERTIFICATE_PEM.with(|inner| inner.get().map(|v| v.clone()))
-    {
-        return Ok(X509CertificateString {
-            x509_certificate_string: certificate,
-        });
-    }
+    // if the public key is not cached, fetch it from the management canister
+    let request = ManagementCanisterSchnorrPublicKeyRequest {
+        canister_id: None,
+        derivation_path: derivation_path(),
+        key_id: key_id(),
+    };
 
-    // if the certificate is not cached, create it and try to cache it
-
-    let profile = Profile::Root;
-    let serial_number = SerialNumber::from(0u32);
-    let validity = validity();
-
-    let subject = Name::from_str(
-        "CN=Web3 certification authority corporation,O=Web3 ceritifcation authority Inc,C=US",
+    let (res,): (ManagementCanisterSchnorrPublicKeyReply,) = ic_cdk::call(
+        Principal::management_canister(),
+        "schnorr_public_key",
+        (request,),
     )
-    .unwrap();
+    .await
+    .map_err(|e| format!("schnorr_public_key failed {}", e.1))?;
 
-    let algorithm = AlgorithmIdentifier::<der::Any> {
-        oid: ed25519::pkcs8::ALGORITHM_OID,
-        parameters: None,
-    };
-
-    let subject_public_key = der::asn1::BitString::new(0, root_ca_public_key_bytes().await?)
-        .map_err(|e| format!("source: {:?}", e.source()))?;
-
-    let pub_key = SubjectPublicKeyInfoOwned {
-        algorithm,
-        subject_public_key,
-    };
-
-    let newly_constructed_x509_certificate_string =
-        pem_certificated_signed_by_root_ca(profile, serial_number, validity, subject, pub_key)
-            .await
-            .map_err(|e| format!("failed to create root certificate: {e:?}"))?;
-
-    let x509_certificate_string = ROOT_CA_CERTIFICATE_PEM.with(move |inner| {
-        inner
-            .get_or_init(|| newly_constructed_x509_certificate_string)
-            .clone()
-    });
-
-    Ok(X509CertificateString {
-        x509_certificate_string,
-    })
+    // try to initialize the cache with the fetched public key or returne the
+    // cached value, because we were making an async call between the cache
+    // check and cache initialization
+    Ok(ROOT_CA_PUBLIC_KEY.with(move |inner| inner.get_or_init(|| res.public_key).clone()))
 }
 
 async fn pem_certificated_signed_by_root_ca(
@@ -234,109 +302,6 @@ async fn pem_certificated_signed_by_root_ca(
     certificate
         .to_pem(LineEnding::LF)
         .map_err(|e| format!("failed to encode certificate: {e:?}"))
-}
-
-fn validity() -> Validity {
-    let not_before_system_time = std::time::SystemTime::UNIX_EPOCH
-        .checked_add(std::time::Duration::from_nanos(time()))
-        .expect("failed to obtain current time");
-    let ten_years = Duration::from_secs(10 * 365 * 24 * 60 * 60);
-    Validity {
-        not_before: Time::try_from(not_before_system_time).expect("failed to convert time"),
-        not_after: Time::try_from(not_before_system_time + ten_years)
-            .expect("failed to convert time"),
-    }
-}
-
-#[update]
-async fn root_ca_public_key() -> Result<PublicKeyReply, String> {
-    let public_key_bytes = root_ca_public_key_bytes().await?;
-    Ok(PublicKeyReply {
-        public_key_hex: hex::encode(&public_key_bytes),
-    })
-}
-
-async fn root_ca_public_key_bytes() -> Result<Vec<u8>, String> {
-    // if the public key is already cached, return it
-    if let Some(public_key) = ROOT_CA_PUBLIC_KEY.with(|inner| inner.get().map(|v| v.clone())) {
-        return Ok(public_key);
-    };
-
-    // if the public key is not cached, fetch it from the management canister
-    let request = ManagementCanisterSchnorrPublicKeyRequest {
-        canister_id: None,
-        derivation_path: derivation_path(),
-        key_id: key_id(),
-    };
-
-    let (res,): (ManagementCanisterSchnorrPublicKeyReply,) = ic_cdk::call(
-        Principal::management_canister(),
-        "schnorr_public_key",
-        (request,),
-    )
-    .await
-    .map_err(|e| format!("schnorr_public_key failed {}", e.1))?;
-
-    // try to initialize the cache with the fetched public key or returne the
-    // cached value, because we were making an async call between the cache
-    // check and cache initialization
-    Ok(ROOT_CA_PUBLIC_KEY.with(move |inner| inner.get_or_init(|| res.public_key).clone()))
-}
-
-#[derive(CandidType, Deserialize, Debug)]
-pub struct PemCertificateRequest {
-    pem_certificate_request: String,
-}
-
-#[update]
-async fn child_certificate(
-    certificate_request_info: PemCertificateRequest,
-) -> Result<X509CertificateString, String> {
-    let cert_req =
-        CertReq::from_pem(certificate_request_info.pem_certificate_request.as_bytes())
-            .map_err(|e| format!("failed to parse PEM certificate signing request: {e:?}"))?;
-
-    verify_certificate_request_signature(&cert_req)?;
-
-    if !cert_req.info.attributes.is_empty() {
-        return Err("Attributes are currently not supported in this example".to_string());
-    }
-
-    prove_ownership(&cert_req, ic_cdk::api::caller() /*, ... */)?;
-
-    let root_certificate_pem = root_ca_certificate().await?;
-    let root_certificate =
-        x509_cert::Certificate::from_pem(root_certificate_pem.x509_certificate_string.as_str())
-            .map_err(|e| format!("failed to parse PEM root CA certificate: {e:?}"))?;
-
-    let profile = Profile::Leaf {
-        issuer: root_certificate.tbs_certificate.subject.clone(),
-        enable_key_agreement: false,
-        enable_key_encipherment: false,
-    };
-
-    // TODO: increment serial number
-    let serial_number = SerialNumber::from(0u32);
-
-    let x509_certificate_string = pem_certificated_signed_by_root_ca(
-        profile,
-        serial_number,
-        // For simplicity of this example, let's just use the same validity
-        // period as the root certificate. In a real application, the validity
-        // would normally not start in the past and might end well before the root
-        // certificate validity ends. Also, the validity of the child
-        // ceritifcate should always be in the time frame of the root
-        // certificate's validity.
-        root_certificate.tbs_certificate.validity.clone(),
-        cert_req.info.subject.clone(),
-        cert_req.info.public_key.clone(),
-    )
-    .await
-    .map_err(|e| format!("failed to create child certificate: {e:?}"))?;
-
-    Ok(X509CertificateString {
-        x509_certificate_string,
-    })
 }
 
 fn verify_certificate_request_signature(certificate_request: &CertReq) -> Result<(), String> {
@@ -387,11 +352,52 @@ fn verify_ed25519_signature(
         .map_err(|e| format!("invalid signature: {:?}", e))
 }
 
+fn next_child_certificate_serial_number() -> u32 {
+    CHILD_CERTIFICATE_SERIAL_NUMBER.with(|state| {
+        let mut serial_number = state.borrow_mut();
+        *serial_number += 1;
+        *serial_number
+    })
+}
+
 fn prove_ownership(_cert_req: &CertReq, _caller: Principal /*, ... */) -> Result<(), String> {
     // ********** This is a placeholder for a real implementation. **********
     // ********** In a real implementation, the subject would have **********
     // ********** to prove ownership of the subject name.          **********
     Ok(())
+}
+
+fn key_id() -> SchnorrKeyId {
+    KEY_NAME.with(|state| {
+        let name = String::from(<&'static str>::from(*state.borrow()));
+        SchnorrKeyId {
+            algorithm: SchnorrAlgorithm::Ed25519,
+            name,
+        }
+    })
+}
+
+fn derivation_path() -> Vec<Vec<u8>> {
+    vec![]
+}
+
+fn validity() -> Validity {
+    let not_before_system_time = std::time::SystemTime::UNIX_EPOCH
+        .checked_add(std::time::Duration::from_nanos(time()))
+        .expect("failed to obtain current time");
+    let ten_years = Duration::from_secs(10 * 365 * 24 * 60 * 60);
+    Validity {
+        not_before: Time::try_from(not_before_system_time).expect("failed to convert time"),
+        not_after: Time::try_from(not_before_system_time + ten_years)
+            .expect("failed to convert time"),
+    }
+}
+
+fn public_key_algorithm_identifier() -> AlgorithmIdentifier<der::Any> {
+    AlgorithmIdentifier::<der::Any> {
+        oid: ed25519::pkcs8::ALGORITHM_OID,
+        parameters: None,
+    }
 }
 
 getrandom::register_custom_getrandom!(always_fail);
