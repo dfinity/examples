e1468d020a1af36472d045e2ac545fc794a8b4f7
Kai Peacock <kylpeacock@gmail.com>
2024-10-24 12:05:00 -0700

    chore: updates internet_identity_integration with best practices

--- motoko/internet_identity_integration/src/greet_frontend/src/index.js
+++ motoko/internet_identity_integration/src/greet_frontend/src/index.js
@@ -1,48 +1,62 @@
-import {createActor, greet_backend} from "../../declarations/greet_backend";
-import {AuthClient} from "@dfinity/auth-client"
-import {HttpAgent} from "@dfinity/agent";
+import { createActor, greet_backend } from "../../declarations/greet_backend";
+import { AuthClient } from "@dfinity/auth-client";
+import { HttpAgent } from "@dfinity/agent";
 
-let actor = greet_backend;
+/**
+ * Put this on the window so we can access it from the console.
+ * This is helpful for learning, but it's recommended to keep it in a closure in a real app.
+ *
+ * We can use the default (not authenticated) greet_backend actor to call the greet method, and then update it once the authClient is ready, and again when the user logs in.
+ */
+window.greetActor = greet_backend;
 
 const greetButton = document.getElementById("greet");
-greetButton.onclick = async (e) => {
-    e.preventDefault();
-
-    greetButton.setAttribute("disabled", true);
-
-    // Interact with backend actor, calling the greet method
-    const greeting = await actor.greet();
-
-    greetButton.removeAttribute("disabled");
-
-    document.getElementById("greeting").innerText = greeting;
-
-    return false;
-};
-
 const loginButton = document.getElementById("login");
-loginButton.onclick = async (e) => {
-    e.preventDefault();
 
-    // create an auth client
-    let authClient = await AuthClient.create();
+AuthClient.create().then((client) => {
+  // Put this on the window so we can access it from the console. This is helpful for learning, but it's recommended to keep it in a closure in a real app.
+  window.authClient = client;
+  // We can use the authClient to check if the user is already authenticated
+  setActor(client);
 
-    // start the login process and wait for it to finish
-    await new Promise((resolve) => {
-        authClient.login({
-            identityProvider: process.env.II_URL,
-            onSuccess: resolve,
-        });
-    });
+  // Now that the auth client is created, we can enable the login and greet buttons
+  loginButton.removeAttribute("disabled");
+  greetButton.removeAttribute("disabled");
+});
 
-    // At this point we're authenticated, and we can get the identity from the auth client:
-    const identity = authClient.getIdentity();
-    // Using the identity obtained from the auth client, we can create an agent to interact with the IC.
-    const agent = new HttpAgent({identity});
-    // Using the interface description of our webapp, we create an actor that we use to call the service methods.
-    actor = createActor(process.env.GREET_BACKEND_CANISTER_ID, {
-        agent,
-    });
-
-    return false;
+loginButton.onclick = async () => {
+  // start the login process and wait for it to finish
+  window.authClient.login({
+    identityProvider: process.env.II_URL,
+    onSuccess: () => {
+      setActor(window.authClient);
+    },
+  });
 };
+
+greetButton.onclick = async () => {
+  // Disable the button to prevent multiple clicks
+  greetButton.setAttribute("disabled", true);
+
+  // Interact with backend actor, calling the greet method
+  const greeting = await window.greetActor.greet();
+
+  greetButton.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+};
+
+/**
+ * Sets the actor to be used by the greet button
+ * @param {AuthClient} an initialized AuthClient, which will have the identity of the user (logged in or not)
+ */
+function setActor(authClient) {
+  // At this point we're authenticated, and we can get the identity from the auth client:
+  const identity = authClient.getIdentity();
+  // Using the identity obtained from the auth client, we can create an agent to interact with the IC.
+  const agent = new HttpAgent({ identity });
+  // Using the interface description of our webapp, we create an actor that we use to call the service methods.
+  window.greetActor = createActor(process.env.GREET_BACKEND_CANISTER_ID, {
+    agent,
+  });
+}
