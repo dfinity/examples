5edc1e42bdb2719332e6c41320b2c99d085b8d45
Adam Spofford <adam.spofford@dfinity.org>
2025-02-18 15:48:29 -0800

    Update dfx to 0.25.0

--- .github/workflows/provision-darwin.sh
+++ .github/workflows/provision-darwin.sh
@@ -18,7 +18,7 @@ rm node.pkg
 
 # Install DFINITY SDK.
 curl --location --output install-dfx.sh "https://raw.githubusercontent.com/dfinity/sdk/master/public/install-dfxvm.sh"
-DFX_VERSION=${DFX_VERSION:=0.24.3} DFXVM_INIT_YES=true bash install-dfx.sh
+DFX_VERSION=${DFX_VERSION:=0.25.0} DFXVM_INIT_YES=true bash install-dfx.sh
 rm install-dfx.sh
 echo "$HOME/Library/Application Support/org.dfinity.dfx/bin" >> $GITHUB_PATH
 source "$HOME/Library/Application Support/org.dfinity.dfx/env"
