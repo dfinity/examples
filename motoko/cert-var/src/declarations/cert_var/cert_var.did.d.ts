3f15aaa508a37b86453bc3b94750bc224af00460
Raymond Khalife <raymond.khalife@dfinity.org>
2025-04-06 09:52:42 -0400

    chore: use standard port for cert-var example

--- motoko/cert-var/src/declarations/cert_var/cert_var.did.d.ts
+++ motoko/cert-var/src/declarations/cert_var/cert_var.did.d.ts
@@ -1,11 +1,14 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
 
 export interface _SERVICE {
   'get' : ActorMethod<
     [],
-    { 'certificate' : [] | [Uint8Array], 'value' : number }
+    { 'certificate' : [] | [Uint8Array | number[]], 'value' : number }
   >,
   'inc' : ActorMethod<[], number>,
   'set' : ActorMethod<[number], undefined>,
 }
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
