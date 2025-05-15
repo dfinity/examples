d9578b0d657b80ae6c6304dec9be19b0f0689330
Franz-Stefan Preiss <franzstefan@dfinity.org>
2025-05-15 11:24:33 +0200

    Migrate Rust vetKD example to use management canister (#1144)

--- rust/vetkd/src/app_backend/src/lib.rs
+++ rust/vetkd/src/app_backend/src/lib.rs
@@ -1,112 +1,108 @@
-use ic_cdk::update;
-use std::str::FromStr;
-use types::{
-    CanisterId, VetKDCurve, VetKDEncryptedKeyReply, VetKDEncryptedKeyRequest, VetKDKeyId,
-    VetKDPublicKeyReply, VetKDPublicKeyRequest,
+use candid::Principal;
+use ic_cdk::api::msg_caller;
+use ic_cdk::call::Call;
+use ic_cdk::management_canister::{
+    VetKDCurve, VetKDDeriveKeyArgs, VetKDDeriveKeyResult, VetKDKeyId, VetKDPublicKeyArgs,
+    VetKDPublicKeyResult,
 };
-
-mod types;
-
-const VETKD_SYSTEM_API_CANISTER_ID: &str = "s55qq-oqaaa-aaaaa-aaakq-cai";
+use ic_cdk::update;
 
 #[update]
 async fn symmetric_key_verification_key() -> String {
-    let request = VetKDPublicKeyRequest {
+    let request = VetKDPublicKeyArgs {
         canister_id: None,
-        derivation_path: vec![b"symmetric_key".to_vec()],
-        key_id: bls12_381_g2_test_key_1(),
+        context: b"symmetric_key".to_vec(),
+        key_id: bls12_381_g2_dfx_test_key(),
     };
 
-    let (response,): (VetKDPublicKeyReply,) = ic_cdk::api::call::call(
-        vetkd_system_api_canister_id(),
-        "vetkd_public_key",
-        (request,),
-    )
-    .await
-    .expect("call to vetkd_public_key failed");
+    let response: VetKDPublicKeyResult =
+        Call::unbounded_wait(Principal::management_canister(), "vetkd_public_key")
+            .with_arg(request)
+            .await
+            .expect("call to vetkd_public_key failed")
+            .candid()
+            .expect("decoding failed");
 
     hex::encode(response.public_key)
 }
 
 #[update]
-async fn encrypted_symmetric_key_for_caller(encryption_public_key: Vec<u8>) -> String {
+async fn encrypted_symmetric_key_for_caller(transport_public_key: Vec<u8>) -> String {
     debug_println_caller("encrypted_symmetric_key_for_caller");
 
-    let request = VetKDEncryptedKeyRequest {
-        derivation_id: ic_cdk::caller().as_slice().to_vec(),
-        derivation_path: vec![b"symmetric_key".to_vec()],
-        key_id: bls12_381_g2_test_key_1(),
-        encryption_public_key,
+    let request = VetKDDeriveKeyArgs {
+        input: msg_caller().as_slice().to_vec(),
+        context: b"symmetric_key".to_vec(),
+        key_id: bls12_381_g2_dfx_test_key(),
+        transport_public_key,
     };
 
-    let (response,): (VetKDEncryptedKeyReply,) = ic_cdk::api::call::call(
-        vetkd_system_api_canister_id(),
-        "vetkd_derive_encrypted_key",
-        (request,),
-    )
-    .await
-    .expect("call to vetkd_derive_encrypted_key failed");
+    let response: VetKDDeriveKeyResult =
+        Call::unbounded_wait(Principal::management_canister(), "vetkd_derive_key")
+            .with_arg(request)
+            .with_cycles(26_153_846_153)
+            .await
+            .expect("call to vetkd_derive_key failed")
+            .candid()
+            .expect("decoding failed");
 
     hex::encode(response.encrypted_key)
 }
 
 #[update]
 async fn ibe_encryption_key() -> String {
-    let request = VetKDPublicKeyRequest {
+    let request = VetKDPublicKeyArgs {
         canister_id: None,
-        derivation_path: vec![b"ibe_encryption".to_vec()],
-        key_id: bls12_381_g2_test_key_1(),
+        context: b"ibe_encryption".to_vec(),
+        key_id: bls12_381_g2_dfx_test_key(),
     };
 
-    let (response,): (VetKDPublicKeyReply,) = ic_cdk::api::call::call(
-        vetkd_system_api_canister_id(),
-        "vetkd_public_key",
-        (request,),
-    )
-    .await
-    .expect("call to vetkd_public_key failed");
+    let response: VetKDPublicKeyResult =
+        Call::unbounded_wait(Principal::management_canister(), "vetkd_public_key")
+            .with_arg(request)
+            .await
+            .expect("call to vetkd_public_key failed")
+            .candid()
+            .expect("decoding failed");
 
     hex::encode(response.public_key)
 }
 
 #[update]
-async fn encrypted_ibe_decryption_key_for_caller(encryption_public_key: Vec<u8>) -> String {
+async fn encrypted_ibe_decryption_key_for_caller(transport_public_key: Vec<u8>) -> String {
     debug_println_caller("encrypted_ibe_decryption_key_for_caller");
 
-    let request = VetKDEncryptedKeyRequest {
-        derivation_id: ic_cdk::caller().as_slice().to_vec(),
-        derivation_path: vec![b"ibe_encryption".to_vec()],
-        key_id: bls12_381_g2_test_key_1(),
-        encryption_public_key,
+    let request = VetKDDeriveKeyArgs {
+        input: msg_caller().as_slice().to_vec(),
+        context: b"ibe_encryption".to_vec(),
+        key_id: bls12_381_g2_dfx_test_key(),
+        transport_public_key,
     };
 
-    let (response,): (VetKDEncryptedKeyReply,) = ic_cdk::api::call::call(
-        vetkd_system_api_canister_id(),
-        "vetkd_derive_encrypted_key",
-        (request,),
-    )
-    .await
-    .expect("call to vetkd_derive_encrypted_key failed");
+    let response: VetKDDeriveKeyResult =
+        Call::unbounded_wait(Principal::management_canister(), "vetkd_derive_key")
+            .with_arg(request)
+            .with_cycles(26_153_846_153)
+            .await
+            .expect("call to vetkd_derive_key failed")
+            .candid()
+            .expect("decoding failed");
 
     hex::encode(response.encrypted_key)
 }
 
-fn bls12_381_g2_test_key_1() -> VetKDKeyId {
+fn bls12_381_g2_dfx_test_key() -> VetKDKeyId {
     VetKDKeyId {
         curve: VetKDCurve::Bls12_381_G2,
-        name: "test_key_1".to_string(),
+        name: "dfx_test_key".to_string(),
     }
-}
-
-fn vetkd_system_api_canister_id() -> CanisterId {
-    CanisterId::from_str(VETKD_SYSTEM_API_CANISTER_ID).expect("failed to create canister ID")
 }
 
 fn debug_println_caller(method_name: &str) {
     ic_cdk::println!(
         "{}: caller: {} (isAnonymous: {})",
         method_name,
-        ic_cdk::caller().to_text(),
-        ic_cdk::caller() == candid::Principal::anonymous()
+        ic_cdk::api::msg_caller().to_text(),
+        ic_cdk::api::msg_caller() == Principal::anonymous()
     );
 }
