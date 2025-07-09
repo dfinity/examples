53b20db1e4b1383296c1c879428294d7238a6474
Jack Lloyd <jack.lloyd@gmail.com>
2025-07-09 15:17:20 -0400

    chore(crypto): CRP-2579 In basic_ethereum example use ic_secp256k1 (#958)
    This crate has the same derivation functionality of extended_bip32, but does not rely on the threshold signature implementation. Thus it has far fewer dependencies.
    
    The extended_bip32 crate was removed in https://github.com/dfinity/ic/pull/1172

--- rust/basic_ethereum/src/ecdsa.rs
+++ rust/basic_ethereum/src/ecdsa.rs
@@ -1,6 +1,5 @@
 use ic_cdk::api::management_canister::ecdsa::EcdsaPublicKeyResponse;
-use ic_crypto_ecdsa_secp256k1::PublicKey;
-use ic_crypto_extended_bip32::{DerivationPath, ExtendedBip32DerivationResult};
+use ic_secp256k1::{PublicKey, DerivationPath};
 use ic_ethereum_types::Address;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -13,17 +12,14 @@ impl EcdsaPublicKey {
     pub fn derive_new_public_key(
         &self,
         derivation_path: &DerivationPath,
-    ) -> ExtendedBip32DerivationResult<Self> {
-        derivation_path
-            .public_key_derivation(
-                &self.public_key.serialize_sec1(/*compressed=*/ true),
-                &self.chain_code,
-            )
-            .map(|output| Self {
-                public_key: PublicKey::deserialize_sec1(&output.derived_public_key)
-                    .expect("BUG: invalid public key"),
-                chain_code: output.derived_chain_code,
-            })
+    ) -> Self {
+
+        let (dk, cc) = self.public_key.derive_subkey(derivation_path);
+
+        Self {
+            public_key: dk,
+            chain_code: cc.to_vec(),
+        }
     }
 }
 
@@ -47,7 +43,7 @@ impl From<&EcdsaPublicKey> for Address {
     fn from(value: &EcdsaPublicKey) -> Self {
         let key_bytes = value.as_ref().serialize_sec1(/*compressed=*/ false);
         debug_assert_eq!(key_bytes[0], 0x04);
-        let hash = ic_crypto_sha3::Keccak256::hash(&key_bytes[1..]);
+        let hash = ic_sha3::Keccak256::hash(&key_bytes[1..]);
         let mut addr = [0u8; 20];
         addr[..].copy_from_slice(&hash[12..32]);
         Address::new(addr)
