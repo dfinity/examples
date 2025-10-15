bad262d15b9277801aeb8cb3e10b3e96c43908ae
Islam El-Ashi <islam.elashi@dfinity.org>
2025-10-15 15:31:28 +0200

    add subaccount

--- rust/receiving-icp/src/lib.rs
+++ rust/receiving-icp/src/lib.rs
@@ -5,16 +5,21 @@ use ic_ledger_types::{AccountBalanceArgs, AccountIdentifier, Subaccount};
 // To use real ICP, use `ryjl3-tyaaa-aaaaa-aaaba-cai` instead.
 const LEDGER_PRINCIPAL: &str = "xafvr-biaaa-aaaai-aql5q-cai";
 
-/// Retrieves the canister's account identifier.
+/// Retrieves the canister's main account.
 #[ic_cdk::query]
-async fn account_identifier() -> String {
+async fn account() -> String {
     AccountIdentifier::new(&ic_cdk::api::canister_self(), &Subaccount([0; 32])).to_string()
 }
 
-/// Retrieves the canister's principal.
+/// Retrieves the canister's subaccount based on upper and lower values.
 #[ic_cdk::query]
-async fn principal() -> Principal {
-    ic_cdk::api::canister_self()
+async fn subaccount(upper: u128, lower: u128) -> String {
+    // Create a 32-byte array by combining the little endian representation of upper and lower
+    let mut subaccount_bytes = [0u8; 32];
+    subaccount_bytes[0..16].copy_from_slice(&upper.to_le_bytes());
+    subaccount_bytes[16..32].copy_from_slice(&lower.to_le_bytes());
+
+    AccountIdentifier::new(&ic_cdk::api::canister_self(), &Subaccount(subaccount_bytes)).to_string()
 }
 
 /// Retrieves own balance from the ledger.
