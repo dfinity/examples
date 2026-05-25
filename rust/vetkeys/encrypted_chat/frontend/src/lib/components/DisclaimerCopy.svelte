87e16c712076e3e4486cf5d9be13021633e9f5a5
Raymond Khalife <raymond.khalife@dfinity.org>
2026-05-25 17:19:25 -0400

    remove motoko/vetkeys

--- rust/vetkeys/encrypted_chat/frontend/src/lib/components/DisclaimerCopy.svelte
+++
@@ -1,4 +0,0 @@
-<script lang="ts"></script>
-
-<strong>Disclaimer:</strong> This sample dapp is intended exclusively for experimental purpose. You are
-advised not to use this dapp for storing your critical data such as keys or passwords.
